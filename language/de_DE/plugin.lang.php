51c252f4cb5a551ac68bd3d7e47bdda14ed3fa4c
ddtddt <ddtddt@piwigo.org>
2009-07-27 15:27:27 +0000

    Add DE to AdditionalPages extension thanks to Alexander
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@3691 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+
+$lang['ap_add_page'] = 'Seite hinzufügen';
+
+$lang['ap_create'] = 'Neue Seite hinzufügen';
+$lang['ap_deleted_page'] = 'Seite gelöscht (<a href="' .PHPWG_ROOT_PATH . '">zurück zur Startseite</a>)';
+$lang['ap_no_name'] = 'Es wurde kein Name vergeben.';
+$lang['ap_saved_page'] = 'Seite gespeichert (<a href="' .PHPWG_ROOT_PATH . 'index.php?/additional_page/%s">Seite anzeigen</a>)';
+$lang['ap_modify'] = 'Neue Seite bearbeiten';
+$lang['ap_all_lang'] = 'Alle';
+$lang['ap_all_group'] = 'Alle';
+
+$lang['ap_conf_saved'] = 'Einstellungen gespeichert.';
+
+$lang['ap_page_name'] = 'Seitenname';
+$lang['ap_page_lang'] = 'Sprache';
+$lang['ap_default_language'] = 'Standard';
+$lang['ap_page_pos'] = 'Position';
+$lang['ap_authorized_group'] = 'Autorisierte Gruppen';
+$lang['ap_authorized_users'] = 'Autorisierte Benutzer';
+$lang['ap_select_all'] = 'Alle auswählen';
+$lang['ap_unselect_all'] = 'Alle abwählen';
+$lang['ap_guest'] = '(Keine Auswahl für öffentliche Seite)';
+$lang['ap_pos0'] = 'Bei 0 erscheint die Seite nicht im Menü';
+$lang['ap_page_content'] = 'Inhalt';
+$lang['ap_save'] = 'Speichern';
+$lang['ap_delete'] = 'Löschen';
+
+$lang['ap_config'] = 'Einstellungen';
+$lang['ap_menu_name'] = 'Menü-Name';
+$lang['ap_show_menu'] = 'Zeige Block-Menü';
+$lang['ap_redirect'] = 'Automatische Weiterleitung zur gespeicherten Seite';
+$lang['ap_show_home'] = 'Zeige "Zur Startseite"-Link auf zusätzlichen Seiten';
+$lang['ap_show_edit'] = 'Zeige Bearbeiten-Link im Block-Menü';
+$lang['ap_perm'] = 'Zugriffsverwaltung anhand von:';
+$lang['ap_group_perm'] = 'Gruppen';
+$lang['ap_user_perm'] = 'Benutzern';
+$lang['ap_parse_bbcode'] = 'Seiten mit BBCode konvertieren';
+$lang['ap_convert_bbcode_ok'] = 'Konvertierung abgeschlossen.';
+
+?>
