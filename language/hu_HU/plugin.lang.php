5c064a604425d9b8bc6dede9304dbba5c8002f1e
ddtddt <ddtddt@piwigo.org>
2009-11-10 23:12:19 +0000

    [extensions]- additionalPages - add Hu thanks to sámli
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@4248 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+
+$lang['ap_add_page'] = 'Oldal hozzáadása';
+
+$lang['ap_create'] = 'További új oldal létrehozása';
+$lang['ap_deleted_page'] = 'Oldal törlése (<a href="' .PHPWG_ROOT_PATH . '">vissza a főoldalra</a>)';
+$lang['ap_no_name'] = 'Nem létező mező.';
+$lang['ap_saved_page'] = 'Oldal mentve (<a href="' .PHPWG_ROOT_PATH . 'index.php?/additional_page/%s">oldal megtekintése</a>)';
+$lang['ap_modify'] = 'Új oldal módosítása';
+$lang['ap_all_lang'] = 'Összes nyelv';
+$lang['ap_all_group'] = 'Összes csoport';
+
+$lang['ap_conf_saved'] = 'Változások mentve.';
+
+$lang['ap_page_name'] = 'Oldal neve';
+$lang['ap_page_lang'] = 'Nyelv';
+$lang['ap_default_language'] = 'Alapértelmezett';
+$lang['ap_page_pos'] = 'Pozíció';
+$lang['ap_authorized_group'] = 'Engedélyezett csoportok';
+$lang['ap_authorized_users'] = 'Engedélyezett felhasználók';
+$lang['ap_select_all'] = 'Összes kijelölése';
+$lang['ap_unselect_all'] = 'Összes kijelölés törlése';
+$lang['ap_guest'] = '(publikus oldalt hagyja üresen)';
+$lang['ap_pos0'] = '0 esetén nem jelenik meg a menüben';
+$lang['ap_page_content'] = 'Tartalom';
+$lang['ap_save'] = 'Mentés';
+$lang['ap_delete'] = 'Törlés';
+
+$lang['ap_config'] = 'Beállítások';
+$lang['ap_menu_name'] = 'Menü neve';
+$lang['ap_show_menu'] = 'Menü blokkok mutatása';
+$lang['ap_redirect'] = 'Oldal automatikus mentése';
+$lang['ap_show_home'] = 'Mutassa a "Vissza a főoldalra" gombot az oldalakon';
+$lang['ap_show_edit'] = 'Mutassa az oldal módosítást a menüben';
+$lang['ap_perm'] = 'Hozzáférés kezelő aktiválása:';
+$lang['ap_group_perm'] = 'csoportok';
+$lang['ap_user_perm'] = 'felhasználók';
+$lang['ap_parse_bbcode'] = 'BBCode konvertálása';
+$lang['ap_convert_bbcode_ok'] = 'Konvertálás kész.';
+
+?>
