c8ffdf0f5a18b4074f5f2ec6d93cfeaa11b6e614
jpr928 <jpr928@piwigo.org>
2010-04-10 18:45:32 +0000

    update ES_es language
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@5772 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -2,22 +2,22 @@
 
 $lang['ap_add_page'] = 'Añadir una página';
 
-$lang['ap_create'] = 'Crear una nueva página additionelle';
-$lang['ap_deleted_page'] = 'Página suprimida (<a href="' .PHPWG_ROOT_PATH . '">vuelta a la acogida</a>)';
+$lang['ap_create'] = 'Crear una nueva página adiciónal';
+$lang['ap_deleted_page'] = 'Página eliminada (<a href="' .PHPWG_ROOT_PATH . '">volver al inicio</a>)';
 $lang['ap_no_name'] = 'Usted no indicó el nombre.';
 $lang['ap_saved_page'] = 'Página registrada (<a href="' .PHPWG_ROOT_PATH . 'index.php?/additional_page/%s">Ver la página</a>)';
-$lang['ap_modify'] = 'Modificar la página additionelle';
-$lang['ap_all_lang'] = 'Ellas todas';
-$lang['ap_all_group'] = 'Ellos todos';
+$lang['ap_modify'] = 'Modificar la página adiciónal';
+$lang['ap_all_lang'] = 'Todas';
+$lang['ap_all_group'] = 'Todos';
 
 $lang['ap_conf_saved'] = 'Configuración salvaguardada.';
 
 $lang['ap_page_name'] = 'Nombre de la página';
-$lang['ap_page_lang'] = 'Lengua';
+$lang['ap_page_lang'] = 'idioma';
 $lang['ap_default_language'] = 'Por defecto';
 $lang['ap_page_pos'] = 'Posición';
 $lang['ap_authorized_group'] = 'Grupos autorizados';
-$lang['ap_authorized_users'] = 'Utilizadores autorizados';
+$lang['ap_authorized_users'] = 'Usuarios autorizados';
 $lang['ap_select_all'] = 'Seleccionar todo ';
 $lang['ap_unselect_all'] = 'Deseleccionar todo ';
 $lang['ap_guest'] = '(Dejar vacío para una página pública)';
@@ -28,13 +28,13 @@ $lang['ap_delete'] = 'Suprimir';
 
 $lang['ap_config'] = 'Configuración';
 $lang['ap_menu_name'] = 'Nombre del menú';
-$lang['ap_show_menu'] = 'Mostrar el bloque menudo';
+$lang['ap_show_menu'] = 'Mostrar el bloque menú';
 $lang['ap_redirect'] = 'Redirigir automáticamente hacia la página registrada';
 $lang['ap_show_home'] = 'Mostrar el botón de vuelta hacia la acogida sobre las páginas adicionales';
-$lang['ap_show_edit'] = 'Mostrar el lazo de edición de las páginas additionelles en el bloque menudo';
-$lang['ap_perm'] = 'Acelerar la gestión de los derechos de acceso a las páginas additionelles :';
+$lang['ap_show_edit'] = 'Mostrar el lazo de edición de las páginas adiciónales en el bloque menú';
+$lang['ap_perm'] = 'Activar la gestión de los derechos de acceso a las páginas adiciónales :';
 $lang['ap_group_perm'] = 'Por grupos';
-$lang['ap_user_perm'] = 'Por tipo de utilizadores';
+$lang['ap_user_perm'] = 'Por tipo de usuarios';
 $lang['ap_parse_bbcode'] = 'Convertir las páginas ya escritas con BBCode';
 $lang['ap_convert_bbcode_ok'] = 'Conversión efectuada.';
 
