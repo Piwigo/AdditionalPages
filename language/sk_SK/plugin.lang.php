4ea1771b7d411df55577814c8475bdbfaf1bb781
ddtddt <ddtddt@piwigo.org>
2010-10-01 16:37:49 +0000

    [extension] AdditionalPages thanks to dodo / add SK
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@7063 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+
+$lang['ap_add_page'] = 'Pridať stránku';
+
+$lang['ap_create'] = 'Vytvoriť novú prídavnú stránku';
+$lang['ap_deleted_page'] = 'Stránka vymazaná (<a href="' .PHPWG_ROOT_PATH . '">späť na úvodnú stránku</a>)';
+$lang['ap_no_name'] = 'Nevyplnili ste názov.';
+$lang['ap_saved_page'] = 'Stránka uložená (<a href="' .PHPWG_ROOT_PATH . 'index.php?/additional_page/%s">pozrieť stránku</a>)';
+$lang['ap_modify'] = 'Upraviť prídavnú stránku';
+$lang['ap_all_lang'] = 'Všetky';
+$lang['ap_all_group'] = 'Všetky';
+
+$lang['ap_conf_saved'] = 'Konfigurácia uložená.';
+
+$lang['ap_page_name'] = 'Názov stránky';
+$lang['ap_page_lang'] = 'Jazyk';
+$lang['ap_default_language'] = 'Predvolený';
+$lang['ap_page_pos'] = 'Umiestnenie';
+$lang['ap_authorized_group'] = 'Skupiny';
+$lang['ap_authorized_users'] = 'Používatelia';
+$lang['ap_select_all'] = 'Označiť všetko';
+$lang['ap_unselect_all'] = 'Odznačiť všetko';
+$lang['ap_guest'] = '(ponechajte prázdne pre verejnú stránku)';
+$lang['ap_pos0'] = '0 nezobrazí stránku v ponuke';
+$lang['ap_page_content'] = 'Obsah';
+$lang['ap_save'] = 'Uložiť';
+$lang['ap_delete'] = 'Vymazať';
+
+$lang['ap_config'] = 'Konfigurácia';
+$lang['ap_menu_name'] = 'Názov menu';
+$lang['ap_show_menu'] = 'Zobraziť blokové menu';
+$lang['ap_redirect'] = 'Automatické presmerovanie na uloženú stránku';
+$lang['ap_show_home'] = 'Zobraziť tlačidlo "späť na hlavnú stránku" na prídavných stránkach';
+$lang['ap_show_edit'] = 'Zobraziť upravovací link na blokové menu';
+$lang['ap_perm'] = 'Zapnúť Activate autorizačný manažment:';
+$lang['ap_group_perm'] = 'skupinami';
+$lang['ap_user_perm'] = 'typmi používateľov';
+$lang['ap_parse_bbcode'] = 'Konvertovať stránky napísané v BBCode';
+$lang['ap_convert_bbcode_ok'] = 'Konverzia úspešná.';
+?>
