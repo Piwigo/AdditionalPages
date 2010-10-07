c5e750459394fd9e8bd2ba09e573c8215a01d3e8
ddtddt <ddtddt@piwigo.org>
2010-10-07 21:41:45 +0000

    [extension] AdditionalPages - add cs_CZ thanks to eddie
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@7127 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+
+ $lang['ap_add_page'] = 'Přidat stránku';
+
+$lang['ap_create'] = 'Přidat novou stránku';
+$lang['ap_deleted_page'] = 'Stránka byla smazána. (<a href="' .PHPWG_ROOT_PATH . '">zpět domů</a>)';
+$lang['ap_no_name'] = 'Je nutné vyplnit jméno.';
+$lang['ap_saved_page'] = 'Stránka byla uložena (<a href="' .PHPWG_ROOT_PATH . 'index.php?/additional_page/%s">zobrazit stránku</a>)';
+$lang['ap_modify'] = 'Upravit stránku';
+$lang['ap_all_lang'] = 'Všechny';
+$lang['ap_all_group'] = 'Všechny';
+
+$lang['ap_conf_saved'] = 'Konfigurace byla uložena.';
+
+$lang['ap_page_name'] = 'Jméno stránky';
+$lang['ap_page_lang'] = 'Jazyk';
+$lang['ap_default_language'] = 'Defaultní';
+$lang['ap_page_pos'] = 'Pozice';
+$lang['ap_authorized_group'] = 'Authorizované skupiny';
+$lang['ap_authorized_users'] = 'Authorizovaní uživatelé';
+$lang['ap_select_all'] = 'Vybrat vše';
+$lang['ap_unselect_all'] = 'Odvybrat vše';
+$lang['ap_guest'] = '(nechat prázdné pro veřejnou stránku)';
+$lang['ap_pos0'] = '0 znamená neukazovat stránku v menu';
+$lang['ap_page_content'] = 'Obsah';
+$lang['ap_save'] = 'Uložit';
+$lang['ap_delete'] = 'Smazat';
+
+$lang['ap_config'] = 'Konfigurace';
+$lang['ap_menu_name'] = 'Jméno menu';
+$lang['ap_show_menu'] = 'Zobrazit blokové menu';
+$lang['ap_redirect'] = 'Automatické přesměrování na uloženou stránku';
+$lang['ap_show_home'] = 'Zobrazit "zpět domů" na stránkách';
+$lang['ap_show_edit'] = 'Zobrazit link na editaci v blokovém menu';
+$lang['ap_perm'] = 'Aktivace správy autorizací:';
+$lang['ap_group_perm'] = 'podle skupin';
+$lang['ap_user_perm'] = 'podle typů uživatelů';
+$lang['ap_parse_bbcode'] = 'Převést stránky psané pomocí BBCode';
+$lang['ap_convert_bbcode_ok'] = 'Převod proběhl v pořádku.';
+
+?>
