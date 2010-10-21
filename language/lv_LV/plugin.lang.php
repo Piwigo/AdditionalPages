6ef14a0c33724a6b2593bc53e6a95a08ad42b91b
ddtddt <ddtddt@piwigo.org>
2010-10-21 18:53:18 +0000

    [extensions] - AdditionalPages add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@7320 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+
+$lang['ap_add_page'] = 'Pievienot lapu';
+
+$lang['ap_create'] = 'Izveidot jaunu papildus lapu';
+$lang['ap_deleted_page'] = 'Lapa dzēsta (<a href="' .PHPWG_ROOT_PATH . '">atpakaļ uz sākumu</a>)';
+$lang['ap_no_name'] = 'Jūs neesat aizpildījis nosaukuma lauku.';
+$lang['ap_saved_page'] = 'Lapa saglabāta (<a href="' .PHPWG_ROOT_PATH . 'index.php?/additional_page/%s">skatīt lapu</a>)';
+$lang['ap_modify'] = 'Veikt izmaiņas papildus lapā';
+$lang['ap_all_lang'] = 'Visas';
+$lang['ap_all_group'] = 'Visas';
+
+$lang['ap_conf_saved'] = 'Konfigurācija saglabāta.';
+
+$lang['ap_page_name'] = 'Lapas nosaukums';
+$lang['ap_page_lang'] = 'Valoda';
+$lang['ap_default_language'] = 'Pēc noklusējuma';
+$lang['ap_page_pos'] = 'Pozīcija';
+$lang['ap_authorized_group'] = 'Autorizētās grupas';
+$lang['ap_authorized_users'] = 'Autorizētie lietotāji';
+$lang['ap_select_all'] = 'Iezīmēt visu';
+$lang['ap_unselect_all'] = 'Atcelt iezīmēto';
+$lang['ap_guest'] = '(atstāt tukšu priekš publiskās lapas)';
+$lang['ap_pos0'] = 'ja nav, nerādīt lapu izvēlnē';
+$lang['ap_page_content'] = 'Saturs';
+$lang['ap_save'] = 'Saglabāt';
+$lang['ap_delete'] = 'Dzēst';
+
+$lang['ap_config'] = 'Konfigurācija';
+$lang['ap_menu_name'] = 'Izvēlnes nosdaukums';
+$lang['ap_show_menu'] = 'Rādīt blokveida izvēlni';
+$lang['ap_redirect'] = 'Automātiski novirzīt uz saglabāto lapu';
+$lang['ap_show_home'] = 'Rādīt "back home" pogu uz papildus lapām';
+$lang['ap_show_edit'] = 'Rādīt laboto saiti blokveida izvēlnē';
+$lang['ap_perm'] = 'Aktivizēt autorizācijas pārvaldnieku:';
+$lang['ap_group_perm'] = 'pēc grupām';
+$lang['ap_user_perm'] = 'pēc lietotāju tipa';
+$lang['ap_parse_bbcode'] = 'Konvertēt BBCode kodā rakstītās lapas';
+$lang['ap_convert_bbcode_ok'] = 'Konvertācija veiksmīga.';
+?>
