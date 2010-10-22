aa34aeac75718ee3a76f3e9852cd787228eceed7
ddtddt <ddtddt@piwigo.org>
2010-10-22 06:05:26 +0000

    [extensions] - AdditionalPages add ru thanks to yuragal
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@7331 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+
+$lang['ap_add_page'] = 'Добавить страницу';
+
+$lang['ap_create'] = 'Создать новую дополнительную сраницу';
+$lang['ap_deleted_page'] = 'Страница удалена (<a href="' .PHPWG_ROOT_PATH . '">на главную</a>)';
+$lang['ap_no_name'] = 'Введите название страницы';
+$lang['ap_saved_page'] = 'Cтраница сохранена (<a href="' .PHPWG_ROOT_PATH . 'index.php?/additional_page/%s">просмотреть страницу</a>)';
+$lang['ap_modify'] = 'Изменить дополнительную страницу';
+$lang['ap_all_lang'] = 'Все';
+$lang['ap_all_group'] = 'Все';
+
+$lang['ap_conf_saved'] = 'Конфигурация сохранена';
+
+$lang['ap_page_name'] = 'Название страницы';
+$lang['ap_page_lang'] = 'Язык';
+$lang['ap_default_language'] = 'По умолчанию';
+$lang['ap_page_pos'] = 'Положение';
+$lang['ap_authorized_group'] = 'Авторизованные группы пользователей';
+$lang['ap_authorized_users'] = 'Авторизованные пользователи';
+$lang['ap_select_all'] = 'Выбрать все';
+$lang['ap_unselect_all'] = 'Снять выделение';
+$lang['ap_guest'] = '(оставьте пустым для общедоступных страниц)';
+$lang['ap_pos0'] = '0 - не показывать страницу в меню';
+$lang['ap_page_content'] = 'Содержимое';
+$lang['ap_save'] = 'Сохранить';
+$lang['ap_delete'] = 'Удалить';
+
+$lang['ap_config'] = 'Настройки';
+$lang['ap_menu_name'] = 'Название меню';
+$lang['ap_show_menu'] = 'Показывать меню';
+$lang['ap_redirect'] = 'Автоматически перенаправлять на сохраненную страницу';
+$lang['ap_show_home'] = 'Показывать кнопку "На главную" на дополнительных страницах';
+$lang['ap_show_edit'] = 'Показывать ссылку редактирования страницы в меню';
+$lang['ap_perm'] = 'Активировать управление авторизацией:';
+$lang['ap_group_perm'] = 'для групп пользователей';
+$lang['ap_user_perm'] = 'для типов пользователей';
+$lang['ap_parse_bbcode'] = 'Конвертировать страницы с BBCode';
+$lang['ap_convert_bbcode_ok'] = 'Конвертирование выполнено';
+
+?>
