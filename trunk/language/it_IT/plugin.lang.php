eafc9ab95b94b501d86a3fae17c21fc4714610bc
riopwg <riopwg@piwigo.org>
2011-05-23 10:43:26 +0000

    Update language IT
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@11004 68402e56-0260-453c-a942-63ccdbb3a9ee

--- trunk/language/it_IT/plugin.lang.php
+++ trunk/language/it_IT/plugin.lang.php
@@ -10,10 +10,10 @@ $lang['ap_modify'] = 'Modificare la pagina addizionale';
 $lang['ap_all_lang'] = 'Tutte';
 $lang['ap_all_group'] = 'Tutti';
 
-$lang['ap_conf_saved'] = 'Configurazione registrata.';
+$lang['ap_conf_saved'] = 'Configurazione registrata';
 
 $lang['ap_page_name'] = 'Nome della pagina';
-$lang['ap_page_lang'] = 'Lingue';
+$lang['ap_page_lang'] = 'Lingua';
 $lang['ap_default_language'] = 'Di default';
 $lang['ap_page_pos'] = 'Posizione';
 $lang['ap_authorized_group'] = 'Gruppi autorizzati';
@@ -30,21 +30,21 @@ $lang['ap_config'] = 'Configurazione';
 $lang['ap_menu_name'] = 'Nome del menù';
 $lang['ap_show_menu'] = 'Mostrare il blocco menù';
 $lang['ap_redirect'] = 'Ridirigere automaticamente verso la pagina registrata';
-$lang['ap_show_home'] = 'Mostrare il pulsante di ritorno verso la home page sulla pagina addizionale';
+$lang['ap_show_home'] = 'Mostrare il pulsante di ritorno verso la Homepage sulla pagina addizionale';
 $lang['ap_show_edit'] = 'Mostrare il link di modifica delle pagine addizionali nel blocco menù';
 $lang['ap_perm'] = 'Attivare la gestione degli accessi alle pagine addizionali :';
 $lang['ap_group_perm'] = 'per gruppo';
 $lang['ap_user_perm'] = 'per tipo d\'utente';
 $lang['ap_parse_bbcode'] = 'Convertire le pagini già scritte con BBCode';
-$lang['ap_convert_bbcode_ok'] = 'Conversione effettuata.';
+$lang['ap_convert_bbcode_ok'] = 'Conversione effettuata';
 
 $lang['ap_edit_page'] = 'Modifica una pagina';
 $lang['ap_set_as_homepage'] = 'Imposta la Homepage';
 $lang['ap_standalone_page'] = 'Pagina standalone';
-$lang['ap_homepage_tip'] = 'Sostituisci la home page della tua galleria';
-$lang['ap_standalone_tip'] = 'La pagina richiede la completa struttura HTML';
-$lang['ap_permalink_already_used'] = 'Permalink %s è già utilizzato da pagina %s';
-/*TODO*/$lang['ap_level_perm'] = 'by privacy level';
-/*TODO*/$lang['ap_language_perm'] = 'by language';
+$lang['ap_homepage_tip'] = 'Sostituisci la Homepage della tua galleria';
+$lang['ap_standalone_tip'] = 'La pagina richiede una struttura HTML completa';
+$lang['ap_permalink_already_used'] = 'Il permalink %s è già utilizzato dalla pagina %s';
+$lang['ap_level_perm'] = 'per livello di privacy';
+$lang['ap_language_perm'] = 'per lingua';
 
 ?>
