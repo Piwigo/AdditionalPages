bc427b35c18855c309f33207520a8b70779aaa4d
ddtddt <ddtddt@piwigo.org>
2011-12-16 07:35:16 +0000

    [extensions] - AdditionalPages - add pt_PT - Português - Portuguese - Thanks to marcelomac
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@12737 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/pt_PT/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+
+
+$lang['ap_add_page'] = 'Inserir uma página';
+
+$lang['ap_create'] = 'Criar nova página ';
+$lang['ap_deleted_page'] = 'Página excluida';
+$lang['ap_no_name'] = 'Preencha o campo "Nome" ';
+$lang['ap_saved_page'] = 'Página gravada';
+$lang['ap_modify'] = 'Modificar página adicional';
+$lang['ap_all_lang'] = 'Tudo';
+$lang['ap_all_group'] = 'Tudo';
+
+$lang['ap_conf_saved'] = 'Configuração gravada';
+
+$lang['ap_page_name'] = 'Nome da página';
+$lang['ap_page_lang'] = 'Idioma';
+$lang['ap_default_language'] = 'Padrão';
+$lang['ap_page_pos'] = 'Posição';
+$lang['ap_authorized_group'] = 'Grupos Autorizados';
+$lang['ap_authorized_users'] = 'Usuários autorizados';
+$lang['ap_select_all'] = 'Selecionar tudo';
+$lang['ap_unselect_all'] = 'Desfazer seleção de todos';
+$lang['ap_guest'] = '(Deixe vazio para página pública)';
+$lang['ap_pos0'] = '0 para não exibir página no menu';
+$lang['ap_page_content'] = 'Conteúdo';
+$lang['ap_save'] = 'Gravar';
+$lang['ap_delete'] = 'Excluir';
+
+$lang['ap_config'] = 'Configuração';
+$lang['ap_menu_name'] = 'Nome do Menu';
+$lang['ap_show_menu'] = 'Mostrar menu em bloco';
+$lang['ap_redirect'] = 'Redirecione automaticamente para a página gravada';
+$lang['ap_show_home'] = 'Mostrar botão "Voltar para o início" nas páginas adicionais';
+$lang['ap_show_edit'] = 'Mostrar link de edição no menu em bloco';
+$lang['ap_perm'] = 'Ativar gerenciamento de autorização';
+$lang['ap_group_perm'] = 'Por grupos';
+$lang['ap_user_perm'] = 'Por tipo de usuário';
+$lang['ap_parse_bbcode'] = 'Converter páginas escritas com BBcode';
+$lang['ap_convert_bbcode_ok'] = 'Conversão realizada com sucesso';
+
+$lang['ap_edit_page'] = 'Editar página';
+$lang['ap_set_as_homepage'] = 'Definir como página Inicial';
+$lang['ap_standalone_page'] = 'Página independente';
+$lang['ap_homepage_tip'] = 'Replace your gallery\'s homepage';
+$lang['ap_standalone_tip'] = 'Página requer estrutura completa em HTML';
+$lang['ap_permalink_already_used'] = 'Permalink %/ já está sendo usado pela página %s';
+$lang['ap_level_perm'] = 'Por nível privado';
+$lang['ap_language_perm'] = 'Por idioma';
+
+?>
