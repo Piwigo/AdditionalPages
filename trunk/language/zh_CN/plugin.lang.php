df3dc4db027bbd8f746fb37246aee0cb01ecb812
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-01-24 08:13:57 +0000

    [Additional Pages] Update zh_CN, thanks to : quqibing
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@12951 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/zh_CN/plugin.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_add_page'] = '添加页面';
+$lang['ap_all_group'] = '所有';
+$lang['ap_all_lang'] = '所有';
+$lang['ap_authorized_group'] = '权限组';
+$lang['ap_authorized_users'] = '权限用户';
+$lang['ap_config'] = '配置';
+$lang['ap_conf_saved'] = '配置已保存';
+$lang['ap_convert_bbcode_ok'] = '转换成功';
+$lang['ap_create'] = '创建一个新的附加页面';
+$lang['ap_default_language'] = '默认';
+$lang['ap_delete'] = '删除';
+$lang['ap_deleted_page'] = '页面已删除';
+$lang['ap_edit_page'] = '编辑页面';
+$lang['ap_group_perm'] = '按组权限';
+$lang['ap_guest'] = '(如果是公共页面则留空)';
+$lang['ap_homepage_tip'] = '替换为相册的首页';
+$lang['ap_language_perm'] = '按语言';
+$lang['ap_level_perm'] = '使用隐私级别';
+$lang['ap_menu_name'] = '菜单名称';
+$lang['ap_modify'] = '修改附加页面';
+$lang['ap_no_name'] = '未填写名称字段';
+$lang['ap_page_content'] = '内容';
+$lang['ap_page_lang'] = '语言';
+$lang['ap_page_name'] = '页面名称';
+$lang['ap_page_pos'] = '位置';
+$lang['ap_parse_bbcode'] = '转换含有BBCode代码的页面';
+$lang['ap_perm'] = '激活权限管理';
+$lang['ap_permalink_already_used'] = '固定链接%s已经被页面%s使用了';
+$lang['ap_pos0'] = '为0则不在菜单中显示页面';
+$lang['ap_redirect'] = '自动重定向到保存的页面';
+$lang['ap_save'] = '保存';
+$lang['ap_saved_page'] = '页面已保存';
+$lang['ap_select_all'] = '全选';
+$lang['ap_set_as_homepage'] = '设置为主页';
+$lang['ap_show_edit'] = '在菜单中显示编辑链接';
+$lang['ap_show_home'] = '在附加页面显示“返回主页”按钮';
+$lang['ap_show_menu'] = '菜单中显示';
+$lang['ap_standalone_page'] = '独立页面';
+$lang['ap_standalone_tip'] = '页面需要有完整的HTML代码结构';
+$lang['ap_unselect_all'] = '全不选';
+$lang['ap_user_perm'] = '按用户类型';
+?>
