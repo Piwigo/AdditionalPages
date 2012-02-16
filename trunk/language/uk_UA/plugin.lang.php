92bd1cd6752c5deb894da7ca5b643cfdd5311796
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-16 12:10:37 +0000

    [Additional Pages] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@13185 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/uk_UA/plugin.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_add_page'] = 'Додати сторінку';
+$lang['ap_all_group'] = 'Всі';
+$lang['ap_all_lang'] = 'Всі';
+$lang['ap_authorized_group'] = 'Авторизованої групи';
+$lang['ap_authorized_users'] = 'Авторизованих користувачів';
+$lang['ap_config'] = 'Конфігурація';
+$lang['ap_conf_saved'] = 'Налаштування збережені.';
+$lang['ap_convert_bbcode_ok'] = 'Перетворення успішно виконані';
+$lang['ap_create'] = 'Створити нову додаткову сторінку';
+$lang['ap_default_language'] = 'За замовчуванням';
+$lang['ap_delete'] = 'Видалити';
+$lang['ap_deleted_page'] = 'Видалити сторінку';
+$lang['ap_edit_page'] = 'Редагувати сторінку';
+$lang['ap_group_perm'] = 'за групами';
+$lang['ap_guest'] = '(Залишити порожнім для публічних сторінок)';
+$lang['ap_homepage_tip'] = 'Замініть домашню сторінку вашої галереї';
+$lang['ap_language_perm'] = 'за мовою';
+$lang['ap_level_perm'] = 'за рівнем конфіденційності';
+$lang['ap_menu_name'] = 'Назва меню';
+$lang['ap_modify'] = 'Змінити додаткові сторінки';
+$lang['ap_no_name'] = 'Ви не заповнили поле Ім\'я.';
+$lang['ap_page_content'] = 'Зміст';
+$lang['ap_page_lang'] = 'Мова';
+$lang['ap_page_name'] = 'Назви сторінок';
+$lang['ap_page_pos'] = 'Положення';
+$lang['ap_parse_bbcode'] = 'Перетворення сторінок, написаних з BBCode';
+$lang['ap_perm'] = 'Увімкніть дозвіл управління:';
+$lang['ap_permalink_already_used'] = 'Постійне посилання %s вже використовується сторінка %s';
+$lang['ap_pos0'] = '0 не показує сторінку меню';
+$lang['ap_redirect'] = 'Автоматичне перенаправлення на збережену сторінку';
+$lang['ap_save'] = 'Зберегти';
+$lang['ap_saved_page'] = 'Сторінка збережена';
+$lang['ap_select_all'] = 'Вибрати всі';
+$lang['ap_set_as_homepage'] = 'Зробити стартовою';
+$lang['ap_show_edit'] = 'Показати посилання редагування на блок меню';
+$lang['ap_show_home'] = 'Показати "повернутись на домашню" на додаткових сторінках';
+$lang['ap_show_menu'] = 'Показати блок меню';
+$lang['ap_standalone_page'] = 'Автономна сторінка';
+$lang['ap_standalone_tip'] = 'Сторінка вимагає повної HTML структури';
+$lang['ap_unselect_all'] = 'Скасувати вибір';
+$lang['ap_user_perm'] = 'за типами користувача';
+?>
