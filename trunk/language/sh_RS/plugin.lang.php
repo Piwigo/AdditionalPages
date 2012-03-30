95175f78985bdbad34cabe304ab2484b4ec6316a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-30 18:30:59 +0000

    [Additional Pages] Add sh_RS, thanks to : vladamilanovic
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@13813 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/sh_RS/plugin.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_add_page'] = 'Dodaj stranu';
+$lang['ap_all_group'] = 'Svi';
+$lang['ap_all_lang'] = 'Sve';
+$lang['ap_authorized_group'] = 'Autorizovane grupe';
+$lang['ap_authorized_users'] = 'Autorizovani korisnici';
+$lang['ap_config'] = 'Podašavanje';
+$lang['ap_conf_saved'] = 'Podešavanje sačuvano.';
+$lang['ap_convert_bbcode_ok'] = 'Konvertovanje uspešno izvršeno';
+$lang['ap_create'] = 'Kreiraj novu dodatnu stranu';
+$lang['ap_default_language'] = 'Podrazumevano';
+$lang['ap_deleted_page'] = 'Strana obrisana';
+$lang['ap_delete'] = 'Obriši';
+$lang['ap_edit_page'] = 'Uredi stranu';
+$lang['ap_group_perm'] = 'po grupama';
+$lang['ap_guest'] = '(ostaviti prazno za javnu stranu)';
+$lang['ap_homepage_tip'] = 'Zameni početnu stranu Vaše galerije';
+$lang['ap_language_perm'] = 'po jeziku';
+$lang['ap_level_perm'] = 'po nivou privatnosti';
+$lang['ap_menu_name'] = 'Naziv menija';
+$lang['ap_modify'] = 'Izmeni dodatnu stranu';
+$lang['ap_no_name'] = 'Niste popunili naziv polja.';
+$lang['ap_page_content'] = 'Sadržaj';
+$lang['ap_page_lang'] = 'Jezik';
+$lang['ap_page_name'] = 'Naziv strane';
+$lang['ap_page_pos'] = 'Pozicija';
+$lang['ap_parse_bbcode'] = 'Konvertuj strane pisane BBCode-om';
+$lang['ap_permalink_already_used'] = 'Trajna veza %s se već koristi kod strane %s';
+$lang['ap_perm'] = 'Uključi upravljanje autorizacijom';
+$lang['ap_redirect'] = 'Automatski prosledi na sačuvanu stranu';
+$lang['ap_saved_page'] = 'Strana sačuvana';
+$lang['ap_save'] = 'Sačuvaj';
+$lang['ap_select_all'] = 'Odaberi sve';
+$lang['ap_set_as_homepage'] = 'Postavi za Početnu stranu';
+$lang['ap_show_edit'] = 'Prikaži uredi link na bloku menija';
+$lang['ap_show_home'] = 'Prikaži "nazad na početnu" dugme na dodatnim stranama';
+$lang['ap_show_menu'] = 'Prikaži blok menija';
+$lang['ap_standalone_page'] = 'Samostalna strana';
+$lang['ap_standalone_tip'] = 'Strana zahteva potpunu HTML strukturu';
+$lang['ap_unselect_all'] = 'Poništi odabir';
+$lang['ap_user_perm'] = 'po tipu korisnika';
+?>
