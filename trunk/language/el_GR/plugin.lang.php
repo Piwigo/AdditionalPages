dfbdf88cf4ef689777b145c33ff67badf99f46c5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-06 07:40:40 +0000

    [additional_pages] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@14791 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/el_GR/plugin.lang.php
@@ -0,0 +1,61 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_add_page'] = 'Προσθέστε μια σελίδα';
+$lang['ap_all_group'] = 'Όλα';
+$lang['ap_all_lang'] = 'Όλα';
+$lang['ap_authorized_group'] = 'Εξουσιοδοτημένες ομάδες';
+$lang['ap_authorized_users'] = 'Εξουσιοδοτημένοι χρήστες';
+$lang['ap_conf_saved'] = 'Η Διαμόρφωση αποθηκεύθηκε.';
+$lang['ap_config'] = 'Διαμόρφωση';
+$lang['ap_convert_bbcode_ok'] = 'Η Μετατροπή έγινε με επιτυχία';
+$lang['ap_create'] = 'Δημιουργία νέας πρόσθετης σελίδας';
+$lang['ap_default_language'] = 'Προεπιλογή';
+$lang['ap_deleted_page'] = 'Η σελίδα διαγράφεται';
+$lang['ap_delete'] = 'Διαγραφή';
+$lang['ap_edit_page'] = 'Επεξεργασία σελίδας';
+$lang['ap_group_perm'] = 'από τις ομάδες';
+$lang['ap_guest'] = '(αφήστε κενή για δημόσια σελίδα)';
+$lang['ap_homepage_tip'] = 'Αντικαταστήστε την αρχική σελίδα της γκαλερί σας';
+$lang['ap_menu_name'] = 'Το όνομα του Μενού';
+$lang['ap_modify'] = 'Τροποποιήστε την πρόσθετη σελίδα';
+$lang['ap_no_name'] = 'Μην συμπληρώστε τον τομέα όνομα.';
+$lang['ap_page_content'] = 'Περιεχόμενα';
+$lang['ap_page_lang'] = 'Γλώσσα';
+$lang['ap_page_name'] = 'Το όνομα της σελίδας';
+$lang['ap_page_pos'] = 'Θέση';
+$lang['ap_parse_bbcode'] = 'Μετατροπή σελίδων γραμμένων με BBCode';
+$lang['ap_permalink_already_used'] = 'ο Μονιμος σύνδεσμος %s χρησιμοποιείται ήδη από τη σελίδα %s';
+$lang['ap_perm'] = 'Ενεργοποιήστε τη διαχείριση αδειών:';
+$lang['ap_pos0'] = '0 για να μην εμφανιστεί η σελίδα στο μενού';
+$lang['ap_redirect'] = 'Αυτόματη ανακατεύθυνση στην αποθηκευμένη σελίδα';
+$lang['ap_saved_page'] = 'Η σελίδα αποθηκεύθηκε';
+$lang['ap_save'] = 'Αποθήκευση';
+$lang['ap_select_all'] = 'Επιλογή όλων';
+$lang['ap_set_as_homepage'] = 'Ορισμός ως αρχική σελίδα';
+$lang['ap_show_edit'] = 'Εμφάνιση του σύνδεσμου επεξεργασίας στο μπλοκ μενού ';
+$lang['ap_show_menu'] = 'Εμφάνιση μπλοκ μενού ';
+$lang['ap_standalone_page'] = 'Αυτόνομη Σελίδα';
+$lang['ap_standalone_tip'] = 'Η Σελίδα απαιτεί πλήρη υποστήριξη δομής HTML';
+$lang['ap_unselect_all'] = 'Αποεπιλογή όλων';
+$lang['ap_user_perm'] = 'από τον τύπο χρήστη';
+?>
