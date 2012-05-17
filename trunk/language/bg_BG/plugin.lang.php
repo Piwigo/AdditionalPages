2bdc36b2fda593b3eb1597f6ab4953540448cf38
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-17 13:31:01 +0000

    [additional_pages] Update bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@15040 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/bg_BG/plugin.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_add_page'] = 'Добавяне на страница';
+$lang['ap_all_group'] = 'Всички';
+$lang['ap_all_lang'] = 'Всички';
+$lang['ap_authorized_group'] = 'Оторизирани групи';
+$lang['ap_authorized_users'] = 'Оторизирани потребинтели';
+$lang['ap_conf_saved'] = 'Конфигурацията е записана.';
+$lang['ap_config'] = 'Конфигурация';
+$lang['ap_convert_bbcode_ok'] = 'Конвертиране завърши успешно';
+$lang['ap_create'] = 'Създаване на нова допълнителна страница';
+$lang['ap_default_language'] = 'По подразбиране';
+$lang['ap_delete'] = 'Изтриване';
+$lang['ap_deleted_page'] = 'Страницата изтрита';
+$lang['ap_edit_page'] = 'Редакция на страница';
+$lang['ap_group_perm'] = 'по групи';
+$lang['ap_guest'] = '(остави празно за публична страница)';
+$lang['ap_homepage_tip'] = 'Замени началната страница на галерията Ви';
+$lang['ap_language_perm'] = 'по език';
+$lang['ap_level_perm'] = 'по ниво на достъп';
+$lang['ap_menu_name'] = 'Име на меню';
+$lang['ap_modify'] = 'Промяна на допълнителна страница';
+$lang['ap_no_name'] = 'Не сте попълнили полето за име.';
+$lang['ap_page_content'] = 'Съдържание';
+$lang['ap_page_lang'] = 'Език';
+$lang['ap_page_name'] = 'Име на страница';
+$lang['ap_page_pos'] = 'Позиция';
+$lang['ap_parse_bbcode'] = 'Конвертиране страници написани с BBC код';
+$lang['ap_permalink_already_used'] = 'Директния линк %s е използван от страница %s';
+$lang['ap_perm'] = 'Активиране на управление за оторизация:';
+$lang['ap_pos0'] = '0 да не се показва на станицата в менюто';
+$lang['ap_redirect'] = 'Автоматично прехвърляне на записана страница';
+$lang['ap_save'] = 'Запис';
+$lang['ap_saved_page'] = 'Страницата е записана';
+$lang['ap_select_all'] = 'Избери всички';
+$lang['ap_set_as_homepage'] = 'Задай начална страница';
+$lang['ap_show_edit'] = 'Покажи редактиращ линк в блок меню';
+$lang['ap_show_home'] = 'Показване на "Към начална" бутона в допълнителните страници';
+$lang['ap_show_menu'] = 'Показване на Блок меню';
+$lang['ap_standalone_page'] = 'Единична страница';
+$lang['ap_standalone_tip'] = 'Страница изискваща пълна HTML структура ';
+$lang['ap_unselect_all'] = 'Отмени избраните';
+$lang['ap_user_perm'] = 'по вид потребител';
+?>
