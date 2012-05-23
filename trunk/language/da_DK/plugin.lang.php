1c5fbb496cda37eda92e5ed602930b8134a307ea
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-23 21:14:32 +0000

    [additional_pages] Update da_DK, thanks to : Egorip
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@15278 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/da_DK/plugin.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_add_page'] = 'Tilføj en side';
+$lang['ap_all_group'] = 'Alle';
+$lang['ap_all_lang'] = 'Alle';
+$lang['ap_authorized_group'] = 'autoriserede grupper';
+$lang['ap_authorized_users'] = 'autoriserede brugere';
+$lang['ap_conf_saved'] = 'Konfiguration gemt.';
+$lang['ap_config'] = 'Konfiguration';
+$lang['ap_convert_bbcode_ok'] = 'Konvertering vellykket udført';
+$lang['ap_create'] = 'Opret ny ekstra side';
+$lang['ap_default_language'] = 'Standard';
+$lang['ap_deleted_page'] = 'siden er slettet';
+$lang['ap_delete'] = 'Slet';
+$lang['ap_edit_page'] = 'Rediger en side';
+$lang['ap_group_perm'] = 'af grupper';
+$lang['ap_guest'] = '(lad stå tom for offentlig side)';
+$lang['ap_homepage_tip'] = 'Udskift dit galleri hjemmeside';
+$lang['ap_language_perm'] = 'af sprog';
+$lang['ap_level_perm'] = 'af privatliv niveau';
+$lang['ap_menu_name'] = 'Menu navne';
+$lang['ap_modify'] = 'Ændre ekstra side';
+$lang['ap_no_name'] = 'Du skal ikke udfylde navnefeltet.';
+$lang['ap_page_content'] = 'Indhold';
+$lang['ap_page_lang'] = 'Sprog';
+$lang['ap_page_name'] = 'Sidens navn';
+$lang['ap_page_pos'] = 'Position';
+$lang['ap_parse_bbcode'] = 'Konverter sider skrevet med BBCode';
+$lang['ap_permalink_already_used'] = 'Permanent link %s bruges allerede af side %s';
+$lang['ap_perm'] = 'Aktiver godkendelse håndtering';
+$lang['ap_pos0'] = '0 til vis ikke side på menuen';
+$lang['ap_redirect'] = 'Automatisk omdirigering til gemt side';
+$lang['ap_saved_page'] = 'siden er gemt';
+$lang['ap_save'] = 'Gem';
+$lang['ap_select_all'] = 'Vælg alt';
+$lang['ap_set_as_homepage'] = 'Sæt som hjemmeside';
+$lang['ap_show_edit'] = 'Vis rediger link på blok menuen';
+$lang['ap_show_home'] = 'Vis "hjem" knappen på flere sider';
+$lang['ap_show_menu'] = 'Vis blok menu';
+$lang['ap_standalone_page'] = 'Selvstændig side';
+$lang['ap_standalone_tip'] = 'Siden kræver fuld HTML struktur';
+$lang['ap_unselect_all'] = 'Fravælg alt';
+$lang['ap_user_perm'] = 'af bruger type';
+?>
