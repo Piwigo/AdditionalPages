4abb5e8bb7ba34f6ad6dd91b740fbd9d95c23ce9
ddtddt <ddtddt@piwigo.org>
2012-07-02 19:49:35 +0000

    [extensions] - AdditionalPages - update fr_CA - Quebec - Québécois - Thanks to TiOger
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@16296 68402e56-0260-453c-a942-63ccdbb3a9ee

--- trunk/language/fr_CA/plugin.lang.php
+++ trunk/language/fr_CA/plugin.lang.php
@@ -1,7 +1,5 @@
 <?php
-
 $lang['ap_add_page'] = 'Ajouter une page';
-
 $lang['ap_create'] = 'Créer une page additionelle';
 $lang['ap_deleted_page'] = 'Page supprimée';
 $lang['ap_no_name'] = 'Vous n\'avez pas indiqué le nom.';
@@ -9,9 +7,7 @@ $lang['ap_saved_page'] = 'Page enregistrée';
 $lang['ap_modify'] = 'Modifier la page additionelle';
 $lang['ap_all_lang'] = 'Toutes';
 $lang['ap_all_group'] = 'Tous';
-
 $lang['ap_conf_saved'] = 'Configuration sauvegardée.';
-
 $lang['ap_page_name'] = 'Nom de la page';
 $lang['ap_page_lang'] = 'Langue';
 $lang['ap_default_language'] = 'Par défaut';
@@ -25,7 +21,6 @@ $lang['ap_pos0'] = '0 pour ne pas afficher la page dans le menu';
 $lang['ap_page_content'] = 'Contenu';
 $lang['ap_save'] = 'Enregistrer';
 $lang['ap_delete'] = 'Supprimer';
-
 $lang['ap_config'] = 'Configuration';
 $lang['ap_menu_name'] = 'Nom du menu';
 $lang['ap_show_menu'] = 'Montrer le bloc menu';
@@ -37,7 +32,6 @@ $lang['ap_group_perm'] = 'par groupes';
 $lang['ap_user_perm'] = 'par type d\'utilisateurs';
 $lang['ap_parse_bbcode'] = 'Convertir les pages déjà écrites avec le BBCode';
 $lang['ap_convert_bbcode_ok'] = 'Conversion effectuée.';
-
 $lang['ap_edit_page'] = 'Éditer une page';
 $lang['ap_set_as_homepage'] = 'Définir comme page d\'accueil';
 $lang['ap_standalone_page'] = 'Page autonome';
@@ -45,6 +39,7 @@ $lang['ap_homepage_tip'] = 'Remplace la page d\'accueil de votre galerie';
 $lang['ap_standalone_tip'] = 'La page requiert une structure HTML complète';
 $lang['ap_permalink_already_used'] = 'Le lien permanent %s est déja utilisé par la page %s';
 $lang['ap_level_perm'] = 'par niveau de confidentialité';
-$lang['ap_language_perm'] = 'par langue'; 
-
+$lang['ap_language_perm'] = 'par langue';
+$lang['Load a page model'] = 'Charger un modèle de page';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Le contenu de la page a changé, êtes-vous sûr de vouloir quitter sans sauvegarder ?'; 
 ?>
