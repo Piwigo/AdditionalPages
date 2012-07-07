9165fea4191cb2a4e160bfc9fe1d654d98248cd3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-07 22:39:11 +0000

    [additional_pages] Update sv_SE, thanks to : styxsailor
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@16470 68402e56-0260-453c-a942-63ccdbb3a9ee

--- trunk/language/sv_SE/plugin.lang.php
+++ trunk/language/sv_SE/plugin.lang.php
@@ -3,10 +3,10 @@
 $lang['ap_add_page'] = 'Lägg till en sida';
 
 $lang['ap_create'] = 'Skapa ny extern sida';
-$lang['ap_deleted_page'] = 'Sidan borttagen (<a href="' .PHPWG_ROOT_PATH . '">Tillbaka</a>)';
-$lang['ap_no_name'] = 'Du fyllede inte i namnfältet.';
-$lang['ap_saved_page'] = 'Sidan sparad (<a href="' .PHPWG_ROOT_PATH . 'index.php?/additional_page/%s">Visa sidan</a>)';
-$lang['ap_modify'] = 'Ändra externa sidan';
+$lang['ap_deleted_page'] = 'Sidan raderad';
+$lang['ap_no_name'] = 'Du fyllde inte i namnfältet.';
+$lang['ap_saved_page'] = 'Sidan sparad';
+$lang['ap_modify'] = 'Ändra den extra sidan';
 $lang['ap_all_lang'] = 'Alla';
 $lang['ap_all_group'] = 'Alla';
 
@@ -14,29 +14,29 @@ $lang['ap_conf_saved'] = 'Konfiguration sparad.';
 
 $lang['ap_page_name'] = 'Sidans namn';
 $lang['ap_page_lang'] = 'Språk';
-$lang['ap_default_language'] = 'standard';
+$lang['ap_default_language'] = 'Standard';
 $lang['ap_page_pos'] = 'Placering';
-$lang['ap_authorized_group'] = 'Auktoriserad grupper';
+$lang['ap_authorized_group'] = 'Auktoriserade grupper';
 $lang['ap_authorized_users'] = 'Auktoriserad användare';
 $lang['ap_select_all'] = 'Välj alla';
-$lang['ap_unselect_all'] = 'Välj inge';
-$lang['ap_guest'] = '(lämnas tom för offentliga sidan)';
-$lang['ap_pos0'] = '0 till inte visa sida på menyn';
+$lang['ap_unselect_all'] = 'Välj inga';
+$lang['ap_guest'] = '(lämna tom för offentliga sidor)';
+$lang['ap_pos0'] = '0 för att inte visa sidan på menyn';
 $lang['ap_page_content'] = 'Inehåll';
 $lang['ap_save'] = 'Spara';
 $lang['ap_delete'] = 'Radera';
 
 $lang['ap_config'] = 'Konfiguration';
-$lang['ap_menu_name'] = 'Meny namn';
-$lang['ap_show_menu'] = 'Visa block menyn';
-$lang['ap_redirect'] = 'Automatiskt hänvisar till sparade sidan';
-$lang['ap_show_home'] = 'Visar "hemma"-knappen på flera sidor';
-$lang['ap_show_edit'] = 'Visa redigera länken på block menyn';
-$lang['ap_perm'] = 'Aktivera tillstånd för hantering:';
-$lang['ap_group_perm'] = 'av grupper';
-$lang['ap_user_perm'] = 'av användaren typ';
-$lang['ap_parse_bbcode'] = 'konvertera sidor skrivna med BBCode';
-$lang['ap_convert_bbcode_ok'] = 'Konvertering verkstäld ©.';
+$lang['ap_menu_name'] = 'Namn på menyn';
+$lang['ap_show_menu'] = 'Visa blockmenyn';
+$lang['ap_redirect'] = 'Automatisk hänvisning till en sparad sida';
+$lang['ap_show_home'] = 'Visar "tillbaks hem"-knappen på extrasidor';
+$lang['ap_show_edit'] = 'Visa redigera länk på blockmenyn';
+$lang['ap_perm'] = 'Aktivera tillståndshantering:';
+$lang['ap_group_perm'] = 'per grupp';
+$lang['ap_user_perm'] = 'per användartyp';
+$lang['ap_parse_bbcode'] = 'Konvertera sidor skrivna med BBCode';
+$lang['ap_convert_bbcode_ok'] = 'Konvertering genomförd';
 
 $lang['ap_edit_page'] = 'Redigera en sida';
 $lang['ap_set_as_homepage'] = 'Ange som startsida';
@@ -44,7 +44,9 @@ $lang['ap_standalone_page'] = 'Fristående sida';
 $lang['ap_homepage_tip'] = 'Ersätt startsidan för ditt galleri';
 $lang['ap_standalone_tip'] = 'Sidan kräver full HTML struktur';
 $lang['ap_permalink_already_used'] = 'Permalänk %s används redan av sidan %s';
-$lang['ap_level_perm'] = 'enligt säkerhets nivå';
+$lang['ap_level_perm'] = 'per säkerhetsnivå';
 $lang['ap_language_perm'] = 'per språk';
 
+$lang['Load a page model'] = 'ladda en sidmodell';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Sidans innehåll har ändrats, är du säker på att du vill avsluta utan att spara?';
 ?>
