8e36d653142a8b9cdac228341d7b281eac6292d2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-15 08:32:12 +0000

    [additional_pages] Update th_TH, thanks to : pradit
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@17570 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/th_TH/plugin.lang.php
@@ -0,0 +1,68 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'เนื้อหาของหน้ามีการแก้ไขเปลี่ยนแปลง คุณแน่ใจหรือไม่ ว่าจะออกโดยไม่บันทึกข้อมูลอะไร';
+$lang['ap_convert_bbcode_ok'] = 'ประสบความสำเร็จในการดำเนินการแปลง';
+$lang['ap_modify'] = 'ปรับเปลี่ยนหน้าเพิ่มเติม';
+$lang['ap_no_name'] = 'คุณไม่ได้กรอกข้อมูลชื่อ';
+$lang['ap_parse_bbcode'] = 'แปลงหน้าเว็บที่เขียนด้วย BBCode';
+$lang['ap_perm'] = 'เปิดใช้งานการจัดการอนุมัติ';
+$lang['ap_permalink_already_used'] = '% s Permalink ถูกใช้ไปแล้วโดยหน้า% s';
+$lang['ap_pos0'] = '0 ถึงไม่แสดงหน้าบนเมนู';
+$lang['ap_redirect'] = 'กำลังเปลี่ยนเส้นทางโดยอัตโนมัติไปยังหน้าบันทึกไว้';
+$lang['ap_set_as_homepage'] = 'ตั้งเป็นหน้าโฮมเพจ';
+$lang['ap_show_edit'] = 'แสดงปุ่มแก้ไข บนเมนูบล็อก';
+$lang['ap_show_home'] = 'แสดงปุ่ม "ย้อนกลับ" บนหน้าเพิ่มเติม';
+$lang['ap_show_menu'] = 'แสดงกล่อเมนู';
+$lang['ap_standalone_page'] = 'หน้าเดี่ยว';
+$lang['ap_standalone_tip'] = 'หน้า ที่ต้องมีโครงสร้าง HTML เต็มรูปแบบ';
+$lang['ap_unselect_all'] = 'ไม่เลือกทั้งหมด';
+$lang['ap_user_perm'] = 'ตามประเภทของผู้ใช้';
+$lang['ap_create'] = 'สร้างหน้าเพิ่มเติมใหม่';
+$lang['ap_guest'] = 'เว้นว่างเพื่อหน้าหลัก';
+$lang['ap_homepage_tip'] = '
+
+แทนที่หน้าแรกของแกลเลอรี่ของคุณ';
+$lang['ap_level_perm'] = 'โดยระดับความเป็นส่วนตัว';
+$lang['ap_save'] = 'บันทึก';
+$lang['ap_saved_page'] = 'หน้าถูกบันทึกแล้ว';
+$lang['ap_select_all'] = 'เลือกทั้งหมด';
+$lang['Load a page model'] = 'ย้ายรูปแบบหน้า';
+$lang['ap_authorized_group'] = 'กลุ่มที่ได้รับอนุญาต';
+$lang['ap_authorized_users'] = 'ผู้ใช้ที่ได้รับอนุญาต';
+$lang['ap_conf_saved'] = 'บันทึกการตั้งค่าแล้ว';
+$lang['ap_config'] = 'การตั้งค่า';
+$lang['ap_language_perm'] = 'โดยภาษา';
+$lang['ap_menu_name'] = 'ชื่อเมนู';
+$lang['ap_page_name'] = 'ชื่อหน้า';
+$lang['ap_page_content'] = 'เนื้อหา';
+$lang['ap_page_lang'] = 'ภาษา';
+$lang['ap_page_pos'] = 'ตำแหน่ง';
+$lang['ap_add_page'] = 'เพิ่มหน้า';
+$lang['ap_all_group'] = 'ทั้งหมด';
+$lang['ap_all_lang'] = 'ทั้งหมด';
+$lang['ap_default_language'] = 'ค่าปกติ';
+$lang['ap_delete'] = 'ลบ';
+$lang['ap_deleted_page'] = 'หน้าได้ถูกลบ';
+$lang['ap_edit_page'] = 'แก้ไขหน้า';
+$lang['ap_group_perm'] = 'โดยกลุ่ม';
+?>
