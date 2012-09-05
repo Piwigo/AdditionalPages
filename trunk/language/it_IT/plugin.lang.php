0401f1cba9ae9d2b60e7bba61d95085ff5667524
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-05 21:11:27 +0000

    [additional_pages] Update it_IT, thanks to : virgigiole
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@17770 68402e56-0260-453c-a942-63ccdbb3a9ee

--- trunk/language/it_IT/plugin.lang.php
+++ trunk/language/it_IT/plugin.lang.php
@@ -4,13 +4,13 @@ $lang['ap_add_page'] = 'Aggiungere una pagina';
 
 $lang['ap_create'] = 'Creare una nuova pagina addizionale';
 $lang['ap_deleted_page'] = 'Pagina cancellata';
-$lang['ap_no_name'] = 'Dovete indicare un nome';
-$lang['ap_saved_page'] = 'Pagina registrata';
+$lang['ap_no_name'] = 'Devi indicare un nome';
+$lang['ap_saved_page'] = 'Pagina salvata';
 $lang['ap_modify'] = 'Modificare la pagina addizionale';
 $lang['ap_all_lang'] = 'Tutte';
 $lang['ap_all_group'] = 'Tutti';
 
-$lang['ap_conf_saved'] = 'Configurazione registrata';
+$lang['ap_conf_saved'] = 'Configurazione salvata';
 
 $lang['ap_page_name'] = 'Nome della pagina';
 $lang['ap_page_lang'] = 'Lingua';
@@ -20,16 +20,16 @@ $lang['ap_authorized_group'] = 'Gruppi autorizzati';
 $lang['ap_authorized_users'] = 'Utenti autorizzati';
 $lang['ap_select_all'] = 'Selezionare tutto';
 $lang['ap_unselect_all'] = 'Deselezionare tutto';
-$lang['ap_guest'] = '(Lsciare vuoto per una pagina pubblica)';
+$lang['ap_guest'] = '(Lasciare vuoto per una pagina pubblica)';
 $lang['ap_pos0'] = '0 per non visualizzare la pagina nel menù';
 $lang['ap_page_content'] = 'Contenuto';
-$lang['ap_save'] = 'Registrare';
+$lang['ap_save'] = 'Salvare';
 $lang['ap_delete'] = 'Cancellare';
 
 $lang['ap_config'] = 'Configurazione';
 $lang['ap_menu_name'] = 'Nome del menù';
 $lang['ap_show_menu'] = 'Mostrare il blocco menù';
-$lang['ap_redirect'] = 'Ridirigere automaticamente verso la pagina registrata';
+$lang['ap_redirect'] = 'Reindirizzare automaticamente verso la pagina salvata';
 $lang['ap_show_home'] = 'Mostrare il pulsante di ritorno verso la Homepage sulla pagina addizionale';
 $lang['ap_show_edit'] = 'Mostrare il link di modifica delle pagine addizionali nel blocco menù';
 $lang['ap_perm'] = 'Attivare la gestione degli accessi alle pagine addizionali :';
@@ -41,7 +41,7 @@ $lang['ap_convert_bbcode_ok'] = 'Conversione effettuata';
 $lang['ap_edit_page'] = 'Modificare una pagina';
 $lang['ap_set_as_homepage'] = 'Impostare la pagina come Homepage';
 $lang['ap_standalone_page'] = 'Pagina standalone';
-$lang['ap_homepage_tip'] = 'Sostituisce la Homepage della vostra galleria';
+$lang['ap_homepage_tip'] = 'Sostituisce la Homepage della tua galleria';
 $lang['ap_standalone_tip'] = 'La pagina richiede una struttura HTML completa';
 $lang['ap_permalink_already_used'] = 'Il permalink %s è già utilizzato dalla pagina %s';
 $lang['ap_level_perm'] = 'per livello di privacy';
