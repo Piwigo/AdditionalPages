6cffd8703fd23d275b39a3ebdb8a42d2d7241f9d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-16 17:18:36 +0000

    [additional_pages] Update pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@18654 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/pt_BR/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_standalone_tip'] = 'Página requer estrutura completa HTML';
+$lang['ap_unselect_all'] = 'Desmarcar todos';
+$lang['ap_user_perm'] = 'por tipo de usuário';
+$lang['ap_menu_name'] = 'Nome do Menu';
+$lang['ap_modify'] = 'Modifica a página adicional';
+$lang['ap_no_name'] = 'Você não preencheu o nome do campo';
+$lang['ap_page_content'] = 'Conteúdo';
+$lang['ap_page_lang'] = 'Idioma';
+$lang['ap_page_name'] = 'Nome da Página';
+$lang['ap_page_pos'] = 'Posição';
+$lang['ap_parse_bbcode'] = 'Converte páginas escritas com BBCode';
+$lang['ap_perm'] = 'Ativa gerenciamento de autorizações';
+$lang['ap_permalink_already_used'] = 'Permalink %s já está sendo usado pela página %s';
+$lang['ap_pos0'] = '0 para não mostrar página no menu';
+$lang['ap_redirect'] = 'Automaticamente redireciona para página salva';
+$lang['ap_save'] = 'Salvar';
+$lang['ap_saved_page'] = 'Página salva';
+$lang['ap_select_all'] = 'Seleciona tudo';
+$lang['ap_set_as_homepage'] = 'Defina como Página Principal';
+$lang['ap_show_edit'] = 'Mostre link para editar no bloco do menu';
+$lang['ap_show_home'] = 'Mostre botão "voltar para página principal" nas páginas adicionais';
+$lang['ap_show_menu'] = 'Mostra bloco do menu';
+$lang['ap_standalone_page'] = 'Página única';
+$lang['Load a page model'] = 'Baixar um modelo de página';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'O conteúdo da página mudou, você tem certeza que quer sair sem salvar?';
+$lang['ap_add_page'] = 'Adiciona uma página';
+$lang['ap_all_group'] = 'Todas';
+$lang['ap_all_lang'] = 'Todas';
+$lang['ap_authorized_group'] = 'Grupos autorizados';
+$lang['ap_authorized_users'] = 'Usuários autorizados';
+$lang['ap_conf_saved'] = 'Configurações salvas';
+$lang['ap_config'] = 'Configuração';
+$lang['ap_convert_bbcode_ok'] = 'Conversão realizada com sucesso';
+$lang['ap_create'] = 'Cria nova página adicional';
+$lang['ap_default_language'] = 'Padrão';
+$lang['ap_delete'] = 'Apaga';
+$lang['ap_deleted_page'] = 'Página apagada';
+$lang['ap_edit_page'] = 'Edite a página';
+$lang['ap_group_perm'] = 'por grupos';
+$lang['ap_guest'] = '(deixar vazio para página pública)';
+$lang['ap_homepage_tip'] = 'Substitua sua página principal da galeria';
+$lang['ap_language_perm'] = 'por idioma';
+$lang['ap_level_perm'] = 'por nível de privacidade';
+?>
