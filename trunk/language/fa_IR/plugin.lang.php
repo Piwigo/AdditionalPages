731932f30231052033dd2c3a8706be36bb00843b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-19 20:46:55 +0000

    [additional_pages] Update fa_IR, thanks to : Garcia
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@20265 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/fa_IR/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Load a page model'] = 'بارگذاری یک نمونه برگه';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'محتوی درون برگه تغییر کرده است، آیا می‌خواهید بدون ذخیره خارج شوید؟';
+$lang['ap_add_page'] = 'افزودن یک برگه';
+$lang['ap_all_group'] = 'همه';
+$lang['ap_all_lang'] = 'همه';
+$lang['ap_authorized_group'] = 'گروه‌های مجاز';
+$lang['ap_authorized_users'] = 'کاربران مجاز';
+$lang['ap_conf_saved'] = 'پیکربندی‌های ذخیره شدند.';
+$lang['ap_config'] = 'پیکربندی';
+$lang['ap_convert_bbcode_ok'] = 'پیکربندی به درستی انجام شدند';
+$lang['ap_create'] = 'ساخت یک برگه‌ی اضافی';
+$lang['ap_default_language'] = 'پیشفرض';
+$lang['ap_delete'] = 'پاک کردن';
+$lang['ap_deleted_page'] = 'برگه پاک شد';
+$lang['ap_edit_page'] = 'ویرایش یک برگه';
+$lang['ap_group_perm'] = 'با گروه‌ها';
+$lang['ap_guest'] = '(برای برگه‌های همگانی خالی گذاشته شود)';
+$lang['ap_homepage_tip'] = 'جایگزین کردن برگه‌ی اصلی گالریتان';
+$lang['ap_language_perm'] = 'با زبان';
+$lang['ap_level_perm'] = 'با رده‌ی حریم خصوصی';
+$lang['ap_menu_name'] = 'نام منو';
+$lang['ap_modify'] = 'ویرایش برگه‌ی اضافی';
+$lang['ap_no_name'] = 'یک نام بنویسید.';
+$lang['ap_page_content'] = 'محتوی';
+$lang['ap_page_lang'] = 'زبان';
+$lang['ap_page_name'] = 'نام برگه';
+$lang['ap_page_pos'] = 'مکان';
+$lang['ap_parse_bbcode'] = 'برگردانی برگه‌هایی که دارای BBCode می‌باشند';
+$lang['ap_perm'] = 'فعال سازی مدیریت مجاز:';
+$lang['ap_permalink_already_used'] = 'لینک ثابت %s برای برگه‌ی %s می‌باشد';
+$lang['ap_pos0'] = 'برای نمایش ندادن برگه در منو 0 بگذارید';
+$lang['ap_redirect'] = 'هدایت خودکار به برگه‌ی ذخیره شده';
+$lang['ap_save'] = 'ذخیره';
+$lang['ap_saved_page'] = 'برگه ذخیره شد';
+$lang['ap_select_all'] = 'گزینش همه';
+$lang['ap_set_as_homepage'] = 'برگه‌ی اصلی گالری شود';
+$lang['ap_show_edit'] = 'نمایش پیوند ویرایش در بلوک منو';
+$lang['ap_show_home'] = 'نمایش دکمه‌ی "بازگشت به خانه" در برگه‌های اضافی';
+$lang['ap_show_menu'] = 'نمایش بلوک منو';
+$lang['ap_standalone_page'] = 'برگه‌ی آزاد';
+$lang['ap_standalone_tip'] = 'برگه باید ساختار HTML داشته باشد';
+$lang['ap_unselect_all'] = 'گزینش نکردن همه';
+$lang['ap_user_perm'] = 'با نوع کاربر';
+?>
