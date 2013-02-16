d3d1ba0b227939fc3dc059a20767182526e907b6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-16 09:39:51 +0000

    [additional_pages] Update et_EE, thanks to : kristian
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@20775 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/et_EE/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_set_as_homepage'] = 'Vali esileheks';
+$lang['Load a page model'] = 'Laadi lehe žabloon';
+$lang['ap_authorized_group'] = 'Autentitud grupid';
+$lang['ap_authorized_users'] = 'Autentitud kasutajad';
+$lang['ap_convert_bbcode_ok'] = 'Teisendamine õnnestus';
+$lang['ap_group_perm'] = 'gruppi järgi';
+$lang['ap_guest'] = '(jäta tühjaks et määrata avalikuks leheks)';
+$lang['ap_homepage_tip'] = 'Asenda gallerii esileht';
+$lang['ap_standalone_tip'] = 'Leht vajab täielikku HTML struktuuri';
+$lang['ap_unselect_all'] = 'Tühista valik';
+$lang['ap_user_perm'] = 'kasutajaliigi järgi';
+$lang['ap_parse_bbcode'] = 'Teisenda BBCode\'iga koostatud leht';
+$lang['ap_perm'] = 'Võimalda autentimisgruppide haldamist:';
+$lang['ap_pos0'] = '0 (null) ei näita lehte menüüs';
+$lang['ap_show_edit'] = 'Näita toimetamislinki plokkmenüüs';
+$lang['ap_show_home'] = 'Näita "tagasi koju" nuppu lisalehtedel';
+$lang['ap_show_menu'] = 'Näita plokkmenüüd';
+$lang['ap_standalone_page'] = 'Eraldiseisev leht';
+$lang['ap_redirect'] = 'Liigu automaatselt salvestatud lehele';
+$lang['ap_menu_name'] = 'Menüü nimi';
+$lang['ap_modify'] = 'Toimeta lisalehte';
+$lang['ap_no_name'] = 'Nimiväli on täitmata.';
+$lang['ap_page_content'] = 'Sisu';
+$lang['ap_page_lang'] = 'Keel';
+$lang['ap_page_name'] = 'Lehe nimi';
+$lang['ap_page_pos'] = 'Asukoht';
+$lang['ap_permalink_already_used'] = 'Püsilink %s on juba kasutusel (%s)';
+$lang['ap_save'] = 'Salvesta';
+$lang['ap_saved_page'] = 'Leht salvestatud';
+$lang['ap_select_all'] = 'Vali kõik';
+$lang['ap_all_group'] = 'Kõik';
+$lang['ap_all_lang'] = 'Kõik';
+$lang['ap_conf_saved'] = 'Seadistused salvestatud.';
+$lang['ap_config'] = 'Seadistused';
+$lang['ap_create'] = 'Lisa uus leht';
+$lang['ap_default_language'] = 'Vaikimisi';
+$lang['ap_delete'] = 'Kustuta';
+$lang['ap_deleted_page'] = 'Leht kustutatud';
+$lang['ap_edit_page'] = 'Toimeta lehte';
+$lang['ap_language_perm'] = 'keele järgi';
+$lang['ap_level_perm'] = 'privaatsuse tase järgi';
+$lang['ap_add_page'] = 'Lisa lehekülg';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Lehekülje sisus on muudatusi, kas sa tahad väljuda ilma muudatusi salvestamata?';
+?>
