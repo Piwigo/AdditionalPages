2b80e8338acf5fa8f61d8ec9ff9694ce1e13d900
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-09 10:40:26 +0000

    [additional_pages] Add gl_ES, thanks to : damufo
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@21406 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/gl_ES/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_pos0'] = '0 para non amosar a páxina no menú';
+$lang['ap_show_edit'] = 'Amosa-la ligazón de modificar no bloque de menú';
+$lang['ap_show_home'] = 'Amosa-lo botón "volver ó inicio" nas páxinas adicionais';
+$lang['ap_show_menu'] = 'Amosa-lo bloque de menú';
+$lang['ap_redirect'] = 'Redireccionar automaticamente á páxina gardada';
+$lang['ap_save'] = 'Gardar';
+$lang['ap_saved_page'] = 'Páxina gardada';
+$lang['ap_select_all'] = 'Seleccionar todo';
+$lang['ap_set_as_homepage'] = 'Establecer como páxina de inicio';
+$lang['ap_standalone_page'] = 'Páxina independente';
+$lang['ap_modify'] = 'Modifica-la páxina adicional';
+$lang['ap_no_name'] = 'Non enchiche-lo campo do nome.';
+$lang['ap_permalink_already_used'] = 'A ligazón permanente %s xa está as ser usada pola páxina %s';
+$lang['ap_menu_name'] = 'Nome do menú';
+$lang['ap_page_content'] = 'Contido';
+$lang['ap_page_lang'] = 'Idioma';
+$lang['ap_page_name'] = 'Nome da páxina';
+$lang['ap_page_pos'] = 'Posición';
+$lang['ap_parse_bbcode'] = 'Converter páxinas escritas con BBCode';
+$lang['ap_perm'] = 'Activación da xestión da autorización:';
+$lang['Load a page model'] = 'Cargar unha páxina modelo';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'O contido da páxina mudou, queres saír sen gardar?';
+$lang['ap_group_perm'] = 'por grupos';
+$lang['ap_guest'] = '(deixar baleiro para páxina pública)';
+$lang['ap_homepage_tip'] = 'Substituír a páxina de inicio da galería';
+$lang['ap_language_perm'] = 'por idioma';
+$lang['ap_level_perm'] = 'por nivel de privacidade';
+$lang['ap_create'] = 'Crear unha nova páxina adicional';
+$lang['ap_default_language'] = 'Predeterminado';
+$lang['ap_delete'] = 'Suprimir';
+$lang['ap_deleted_page'] = 'Eliminouse a páxina';
+$lang['ap_edit_page'] = 'Modificar páxina';
+$lang['ap_convert_bbcode_ok'] = 'A conversión realizouse con éxito';
+$lang['ap_add_page'] = 'Engadir unha páxia';
+$lang['ap_all_group'] = 'Todo';
+$lang['ap_all_lang'] = 'Todo';
+$lang['ap_authorized_group'] = 'Grupos autorizados';
+$lang['ap_authorized_users'] = 'Usuarios autorizados';
+$lang['ap_conf_saved'] = 'Gardouse a configuración.';
+$lang['ap_config'] = 'Configuración';
+$lang['ap_standalone_tip'] = 'A páxina require unha estrutura completa HTML';
+$lang['ap_unselect_all'] = 'Deseleccionar todo';
+$lang['ap_user_perm'] = 'Por tipo de usuarios';
+?>
