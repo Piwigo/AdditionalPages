533d4d9d2f5c5d267e09921d108206555f31f523
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-16 16:22:45 +0000

    [additional_pages] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@21600 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/zh_TW/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_user_perm'] = '用戶類型';
+$lang['ap_unselect_all'] = '取消選擇所有';
+$lang['ap_standalone_tip'] = '頁面需要完整的HTML結構';
+$lang['ap_standalone_page'] = '單獨頁面';
+$lang['ap_show_menu'] = '顯示區塊選單';
+$lang['ap_show_home'] = '顯示"回首頁"按鈕，在其他頁面';
+$lang['ap_show_edit'] = '顯示編輯鏈接在區塊選單上';
+$lang['ap_set_as_homepage'] = '設為首頁';
+$lang['ap_select_all'] = '選擇所有';
+$lang['ap_saved_page'] = '網頁已儲存';
+$lang['ap_save'] = '儲存';
+$lang['ap_redirect'] = '自動重定向到已儲存的網頁';
+$lang['ap_pos0'] = '0 是不要顯示頁面在上選單';
+$lang['ap_permalink_already_used'] = '固定鏈接 %s 已經被網頁所用';
+$lang['ap_perm'] = '啟動授權管理：';
+$lang['ap_parse_bbcode'] = '轉換BBCode代碼編寫的網頁';
+$lang['ap_page_pos'] = '位置';
+$lang['ap_page_name'] = '頁面的名稱';
+$lang['ap_page_lang'] = '語言';
+$lang['ap_page_content'] = '內容';
+$lang['ap_no_name'] = '您沒有填寫名稱欄位。';
+$lang['ap_modify'] = '修改頁面';
+$lang['ap_menu_name'] = '選單的名稱';
+$lang['ap_level_perm'] = '由隱私級別';
+$lang['ap_language_perm'] = '由語言';
+$lang['ap_homepage_tip'] = '取代畫廊的主頁';
+$lang['ap_guest'] = '(留空用於公開頁面)';
+$lang['ap_group_perm'] = '通過群組';
+$lang['ap_edit_page'] = '編輯頁面';
+$lang['ap_deleted_page'] = '頁面已刪除';
+$lang['ap_delete'] = '刪除';
+$lang['ap_default_language'] = '預設值';
+$lang['ap_create'] = '建立新的額外的頁面';
+$lang['ap_convert_bbcode_ok'] = '轉換成功執行';
+$lang['ap_config'] = '設定';
+$lang['ap_conf_saved'] = '設定已儲存';
+$lang['ap_authorized_users'] = '授權用戶';
+$lang['ap_authorized_group'] = '授權群組';
+$lang['ap_all_lang'] = '所有';
+$lang['ap_all_group'] = '所有';
+$lang['ap_add_page'] = '新加一個網頁';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = '網頁的內容改變了，您確定要退出而不儲存？';
+$lang['Load a page model'] = '載入一個模範頁面';
+?>
