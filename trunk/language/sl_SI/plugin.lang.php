54bbecb70f0af780ecb7899dfd11daa6788bab4a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-18 20:37:09 +0000

    [additional_pages] Add sl_SI, thanks to : MarkoAm
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@21678 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/sl_SI/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_show_edit'] = 'Prikaži "uredi povezavo" na sklopu menija';
+$lang['ap_show_menu'] = 'Prikaži sklop menija';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Vsebina strani je spremenjena. Ste prepričani, da želite zapustiti stran brez shranjevanja?';
+$lang['ap_homepage_tip'] = 'Zamenja domačo stran vaše galerije';
+$lang['ap_show_home'] = 'Prikaži "nazaj domov" na dodatnih straneh';
+$lang['ap_standalone_page'] = 'Samostojna stran';
+$lang['ap_standalone_tip'] = 'Stran zahteva polno HTML strukturo';
+$lang['ap_unselect_all'] = 'Odznači vse';
+$lang['ap_user_perm'] = 'po vrsti uporabnika';
+$lang['ap_pos0'] = 'ne prikaži strani na meniju';
+$lang['ap_redirect'] = 'Samodejno preusmeri na shranjeno stran';
+$lang['ap_save'] = 'Shrani';
+$lang['ap_saved_page'] = 'Stran shranjena';
+$lang['ap_select_all'] = 'Izberi vse';
+$lang['ap_set_as_homepage'] = 'Nastavi kot domačo stran';
+$lang['ap_no_name'] = 'Niste vpisali imena polja';
+$lang['ap_page_content'] = 'Vsebina';
+$lang['ap_page_lang'] = 'Jezik';
+$lang['ap_page_name'] = 'Ime strani';
+$lang['ap_page_pos'] = 'Položaj';
+$lang['ap_parse_bbcode'] = 'Pretvori strani napisane z BBCode';
+$lang['ap_perm'] = 'Vključite dovoljenja za upravljanje:';
+$lang['ap_permalink_already_used'] = 'To stalno povezavo(Permalink) %s že uporablja stran %s';
+$lang['Load a page model'] = 'Naloži modul Strani';
+$lang['ap_create'] = 'Ustvari novo dodatno stran';
+$lang['ap_default_language'] = 'Privzeto';
+$lang['ap_delete'] = 'Izbriši';
+$lang['ap_deleted_page'] = 'Stran izbrisana';
+$lang['ap_edit_page'] = 'Uredi stran';
+$lang['ap_group_perm'] = 'po skupinah';
+$lang['ap_guest'] = '(pusti prazno za javno stran)';
+$lang['ap_language_perm'] = 'po jeziku';
+$lang['ap_level_perm'] = 'po nivoju zasebnosti';
+$lang['ap_menu_name'] = 'Ime Menija';
+$lang['ap_modify'] = 'Spremeni dodatno stran';
+$lang['ap_add_page'] = 'Dodaj stran';
+$lang['ap_all_group'] = 'Vsi';
+$lang['ap_all_lang'] = 'Vse';
+$lang['ap_authorized_group'] = 'Odobrene skupine';
+$lang['ap_authorized_users'] = 'Odobreni uporabniki';
+$lang['ap_conf_saved'] = 'Nastavitve so shranjene';
+$lang['ap_config'] = 'Nastavitve';
+$lang['ap_convert_bbcode_ok'] = 'Pretvorba uspešno izvedena';
+?>
