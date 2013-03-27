070c5e6475caa4b767fa423d9ce716d60a719747
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-27 19:58:57 +0000

    [additional_pages] Add nn_NO, thanks to : kiho
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@21874 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/nn_NO/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_authorized_group'] = 'Autorisera grupper';
+$lang['ap_authorized_users'] = 'Autorisera brukarar';
+$lang['ap_group_perm'] = 'på grupper';
+$lang['ap_homepage_tip'] = 'Bytt ut startsida på galleriet ditt med denne sida';
+$lang['ap_language_perm'] = 'på språk';
+$lang['ap_level_perm'] = 'på personverninnstillingar';
+$lang['ap_perm'] = 'Aktiver autorisasjonhandtering';
+$lang['ap_standalone_tip'] = 'Sida krev full HTML struktir';
+$lang['ap_user_perm'] = 'på brukartype';
+$lang['ap_unselect_all'] = 'Nullstill';
+$lang['ap_convert_bbcode_ok'] = 'Konvertering var vellukka';
+$lang['ap_parse_bbcode'] = 'Konverter sider skrive i BBCode';
+$lang['ap_permalink_already_used'] = 'Permalenke %s er allereie i bruk på side %s';
+$lang['ap_show_home'] = 'Vis "tilbake til startside"-knapp på tilleggssider';
+$lang['ap_redirect'] = 'Vidaresend automatisk til lagra side';
+$lang['ap_create'] = 'Lag ei ny tilleggsside';
+$lang['ap_modify'] = 'Endre tilleggsida';
+$lang['ap_show_menu'] = 'Vis blokk-meny';
+$lang['ap_standalone_page'] = 'Enkeltståande side';
+$lang['ap_no_name'] = 'Du fyllte ikkje ut felt for namn.';
+$lang['ap_pos0'] = '0 for å ikkje vise sida på menyen';
+$lang['ap_save'] = 'Lagre';
+$lang['ap_saved_page'] = 'Sida er lagra';
+$lang['ap_select_all'] = 'Velg alt';
+$lang['ap_set_as_homepage'] = 'Set som startside';
+$lang['ap_show_edit'] = 'Vis lenke for å endre på blokk-menyen';
+$lang['ap_menu_name'] = 'Namn på menyen';
+$lang['ap_page_content'] = 'Innhald';
+$lang['ap_page_lang'] = 'Språk';
+$lang['ap_page_name'] = 'Namn på sida';
+$lang['ap_page_pos'] = 'Posisjon';
+$lang['Load a page model'] = 'Last inn ein sidemodell';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Innhaldet på sida er endra, er du sikker på at du vil avslutte utan å lagre?';
+$lang['ap_add_page'] = 'Legg til side';
+$lang['ap_all_group'] = 'Alle';
+$lang['ap_all_lang'] = 'Alle';
+$lang['ap_conf_saved'] = 'Innstillingane er lagra.';
+$lang['ap_config'] = 'Innstillingar';
+$lang['ap_default_language'] = 'Standard';
+$lang['ap_delete'] = 'Fjern';
+$lang['ap_deleted_page'] = 'Sida er fjerna';
+$lang['ap_edit_page'] = 'Endre ei side';
+$lang['ap_guest'] = '(lat denne vera tom for sider som skal sjåast av alle)';
+?>
