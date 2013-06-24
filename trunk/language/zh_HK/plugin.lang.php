29094c524455b629125728666dbe962287984984
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-24 03:51:15 +0000

    [additional_pages] Add zh_HK, thanks to : gurunited
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@23491 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/zh_HK/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_permalink_already_used'] = '固定鏈接 %s 已經被網頁%s使用';
+$lang['ap_pos0'] = '不要顯示0在頁面選單上';
+$lang['ap_show_edit'] = '在區塊選單顯示編輯鏈接';
+$lang['ap_show_home'] = '在其他頁面顯示"回首頁"按鈕';
+$lang['ap_unselect_all'] = '取消所有選擇';
+$lang['ap_parse_bbcode'] = '轉換網頁至BBCode代碼';
+$lang['ap_redirect'] = '自動轉到已儲存的網頁';
+$lang['Load a page model'] = '載入網頁樣本';
+$lang['ap_group_perm'] = '按照群組';
+$lang['ap_language_perm'] = '按照語言';
+$lang['ap_level_perm'] = '按照私隱級數';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = '網頁內容曾經作出更改, 請確認是否離開及不作儲存?';
+$lang['ap_authorized_group'] = '授權群組';
+$lang['ap_authorized_users'] = '授權的用戶';
+$lang['ap_convert_bbcode_ok'] = '轉換已經完成';
+$lang['ap_guest'] = '(留空給公共網頁)';
+$lang['ap_standalone_tip'] = '頁面需要完整的HTML結構';
+$lang['ap_user_perm'] = '用戶類型';
+$lang['ap_menu_name'] = '選單的名稱';
+$lang['ap_modify'] = '修改頁面';
+$lang['ap_no_name'] = '您沒有填寫名稱欄位';
+$lang['ap_page_content'] = '內容';
+$lang['ap_page_lang'] = '語言';
+$lang['ap_page_name'] = '頁面的名稱';
+$lang['ap_page_pos'] = '位置';
+$lang['ap_perm'] = '啟動授權管理';
+$lang['ap_save'] = '儲存';
+$lang['ap_saved_page'] = '網頁已儲存';
+$lang['ap_select_all'] = '選擇所有';
+$lang['ap_set_as_homepage'] = '設為首頁';
+$lang['ap_show_menu'] = '顯示區塊選單';
+$lang['ap_standalone_page'] = '單獨頁面';
+$lang['ap_add_page'] = '新增一頁';
+$lang['ap_all_group'] = '全部';
+$lang['ap_all_lang'] = '全部';
+$lang['ap_conf_saved'] = '設定已被儲存';
+$lang['ap_config'] = '設定';
+$lang['ap_create'] = '新增一頁';
+$lang['ap_default_language'] = '預設';
+$lang['ap_delete'] = '刪除';
+$lang['ap_deleted_page'] = '網頁已被刪除';
+$lang['ap_edit_page'] = '編輯網頁';
+$lang['ap_homepage_tip'] = '替換你的畫廊主頁';
+?>
