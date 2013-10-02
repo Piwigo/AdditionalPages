e17440a2a79dfde9e13460ac79a3869424654aa2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-02 04:36:01 +0000

    [additional_pages] Add eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@24722 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/eo_EO/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_standalone_tip'] = 'La paĝo bezonas tutan HTML-strukturon';
+$lang['ap_unselect_all'] = 'Malselekti ĉion';
+$lang['ap_user_perm'] = 'laŭ uzantotipo';
+$lang['ap_menu_name'] = 'Menunomo';
+$lang['ap_modify'] = 'Modifi la plian paĝon';
+$lang['ap_no_name'] = 'Vi ne skribis la nomokampon.';
+$lang['ap_page_content'] = 'Enhavo';
+$lang['ap_page_lang'] = 'Lingvo';
+$lang['ap_page_name'] = 'Paĝnomo';
+$lang['ap_page_pos'] = 'Pozicio';
+$lang['ap_parse_bbcode'] = 'Konverti paĝojn skribitajn per BBCode';
+$lang['ap_perm'] = 'Aktivigi rajtiga mastrumado';
+$lang['ap_permalink_already_used'] = 'La konstantan ligilon %s jam uzis la paĝo %s';
+$lang['ap_pos0'] = 'o por ne vidigi la paĝon en la menuo';
+$lang['ap_redirect'] = 'Aŭtomate alidirekti al konservita paĝo';
+$lang['ap_save'] = 'Konservi';
+$lang['ap_saved_page'] = 'Paĝo konservita';
+$lang['ap_select_all'] = 'Selekti ĉion';
+$lang['ap_set_as_homepage'] = 'Ŝalti kiel ĉefpaĝo';
+$lang['ap_show_edit'] = 'Montri la redaktoligilon de la pliajn paĝojn en la menubloko';
+$lang['ap_show_home'] = 'Montri butonon "reveni al la ĉefpaĝo" sur pliajn paĝojn';
+$lang['ap_show_menu'] = 'Montri menublokon';
+$lang['ap_standalone_page'] = 'Memstara paĝo';
+$lang['Load a page model'] = 'Elŝuti paĝoŝablonon';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'La enhavo de la paĝo ŝanĝiĝis. Ĉu vi certe deziras eliri sen konservi?';
+$lang['ap_add_page'] = 'Aldoni paĝon';
+$lang['ap_all_group'] = 'Ĉiuj';
+$lang['ap_all_lang'] = 'Ĉiuj';
+$lang['ap_authorized_group'] = 'Permesitaj grupoj';
+$lang['ap_authorized_users'] = 'Permesitaj uzantoj';
+$lang['ap_conf_saved'] = 'Agordo konserviĝis.';
+$lang['ap_config'] = 'Agordo';
+$lang['ap_convert_bbcode_ok'] = 'Konverto sukcesis';
+$lang['ap_create'] = 'Krei novan plian paĝon';
+$lang['ap_default_language'] = 'Apriora';
+$lang['ap_delete'] = 'Forigi';
+$lang['ap_deleted_page'] = 'La paĝo foriĝis';
+$lang['ap_edit_page'] = 'Redakti paĝon';
+$lang['ap_group_perm'] = 'laŭ grupo';
+$lang['ap_guest'] = '(lasu malplena por publika paĝo)';
+$lang['ap_homepage_tip'] = 'Anstataŭigi vian galerian ĉefpaĝon';
+$lang['ap_language_perm'] = 'laŭ lingvo';
+$lang['ap_level_perm'] = 'laŭ privateca nivelo';
+?>
