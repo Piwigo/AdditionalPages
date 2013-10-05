2ab4db859ce5ee8fb6412a6cdb2453a691c63515
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-05 15:40:34 +0000

    [additional_pages] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@24765 68402e56-0260-453c-a942-63ccdbb3a9ee

--- trunk/language/it_IT/plugin.lang.php
+++ trunk/language/it_IT/plugin.lang.php
@@ -1,12 +1,12 @@
 <?php
 
-$lang['ap_add_page'] = 'Aggiungere una pagina';
+$lang['ap_add_page'] = 'Aggiungi una pagina';
 
-$lang['ap_create'] = 'Creare una nuova pagina addizionale';
+$lang['ap_create'] = 'Crea una nuova pagina addizionale';
 $lang['ap_deleted_page'] = 'Pagina cancellata';
 $lang['ap_no_name'] = 'Non hai compilato il campo nome.';
 $lang['ap_saved_page'] = 'Pagina salvata';
-$lang['ap_modify'] = 'Modificare la pagina aggiuntiva';
+$lang['ap_modify'] = 'Modifica la pagina aggiuntiva';
 $lang['ap_all_lang'] = 'Tutte';
 $lang['ap_all_group'] = 'Tutti';
 
@@ -14,39 +14,39 @@ $lang['ap_conf_saved'] = 'Configurazione salvata';
 
 $lang['ap_page_name'] = 'Nome della pagina';
 $lang['ap_page_lang'] = 'Lingua';
-$lang['ap_default_language'] = 'Di default';
+$lang['ap_default_language'] = 'Impostazione predefinita';
 $lang['ap_page_pos'] = 'Posizione';
 $lang['ap_authorized_group'] = 'Gruppi autorizzati';
 $lang['ap_authorized_users'] = 'Utenti autorizzati';
-$lang['ap_select_all'] = 'Selezionare tutto';
-$lang['ap_unselect_all'] = 'Deselezionare tutto';
-$lang['ap_guest'] = '(lasciare vuoto per una pagina pubblica)';
+$lang['ap_select_all'] = 'Seleziona tutto';
+$lang['ap_unselect_all'] = 'Deseleziona tutto';
+$lang['ap_guest'] = '(lascia vuoto per una pagina pubblica)';
 $lang['ap_pos0'] = '0 per non visualizzare la pagina nel menù';
 $lang['ap_page_content'] = 'Contenuto';
-$lang['ap_save'] = 'Salvare';
-$lang['ap_delete'] = 'Cancellare';
+$lang['ap_save'] = 'Salva';
+$lang['ap_delete'] = 'Cancella';
 
 $lang['ap_config'] = 'Configurazione';
 $lang['ap_menu_name'] = 'Nome del menù';
-$lang['ap_show_menu'] = 'Mostrare il blocco menù';
+$lang['ap_show_menu'] = 'Mostra il blocco menù';
 $lang['ap_redirect'] = 'Reindirizzare automaticamente verso la pagina salvata';
-$lang['ap_show_home'] = 'Mostrare il pulsante di "ritorno alla Homepage" su pagine aggiuntive';
+$lang['ap_show_home'] = 'Mostra il pulsante di "ritorno alla homepage" su pagine aggiuntive';
 $lang['ap_show_edit'] = 'Visualizza link modifica dal blocco menù';
-$lang['ap_perm'] = 'Attivare gestione autorizzazioni:';
+$lang['ap_perm'] = 'Attiva gestione autorizzazioni:';
 $lang['ap_group_perm'] = 'per gruppo';
 $lang['ap_user_perm'] = 'per tipo di utente';
 $lang['ap_parse_bbcode'] = 'Convertire le pagine scritte con BBCode';
 $lang['ap_convert_bbcode_ok'] = 'Conversione eseguita con successo';
 
-$lang['ap_edit_page'] = 'Modificare una pagina';
+$lang['ap_edit_page'] = 'Modifica una pagina';
 $lang['ap_set_as_homepage'] = 'Imposta come Homepage';
 $lang['ap_standalone_page'] = 'Pagina standalone';
-$lang['ap_homepage_tip'] = 'Sostituisce la Homepage della tua galleria';
+$lang['ap_homepage_tip'] = 'Sostituisci la homepage della tua galleria';
 $lang['ap_standalone_tip'] = 'La pagina richiede una struttura HTML completa';
 $lang['ap_permalink_already_used'] = 'Il permalink %s è già utilizzato dalla pagina %s';
 $lang['ap_level_perm'] = 'per livello di privacy';
 $lang['ap_language_perm'] = 'per lingua';
 
-$lang['Load a page model'] = 'Caricare un modello di pagina';
+$lang['Load a page model'] = 'Carica un modello di pagina';
 $lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Il contenuto della pagina è cambiato, sei sicuro di voler uscire senza salvare?';
 ?>
