07da7dc80ea79f7abb0d5e307f1812481e328349
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-19 09:42:25 +0000

    [additional_pages] Add lt_LT, thanks to : vilisas
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@24994 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/lt_LT/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_standalone_tip'] = 'Puslapiui reikalingas pilnas HTML kodas';
+$lang['ap_unselect_all'] = 'Atžymėti viską';
+$lang['ap_user_perm'] = 'pagal vartotojo tipą';
+$lang['ap_menu_name'] = 'Meniu pavadinimas';
+$lang['ap_modify'] = 'Pakeisti papildomą puslapį';
+$lang['ap_no_name'] = 'Įveskite puslapio pavadinimą.';
+$lang['ap_page_content'] = 'Turinys';
+$lang['ap_page_lang'] = 'Kalba';
+$lang['ap_page_name'] = 'Puslapio pavadinimas';
+$lang['ap_page_pos'] = 'Pozicija';
+$lang['ap_parse_bbcode'] = 'Konvertuoti puslapius, parašytus su BB kodu';
+$lang['ap_perm'] = 'Aktyvuoti autorizacijos valdymą';
+$lang['ap_permalink_already_used'] = 'Permalink\'as %s jau naudojamas su puslapiu %s';
+$lang['ap_pos0'] = '0 - nerodyti puslapio meniu juostoje';
+$lang['ap_redirect'] = 'Automatiškai nukreipti į išsaugotą puslapį';
+$lang['ap_save'] = 'Išsaugoti';
+$lang['ap_saved_page'] = 'Puslapis išsaugotas';
+$lang['ap_select_all'] = 'Pažymėti viską';
+$lang['ap_set_as_homepage'] = 'Nustatyti pagrindiniu puslapiu';
+$lang['ap_show_edit'] = 'Rodyti redagavimo nuorodą meniu bloke';
+$lang['ap_show_home'] = 'Rodyti "atgal į pagrindinį" mygtuką papildomuose puslapiuose';
+$lang['ap_show_menu'] = 'Rodyti meniu';
+$lang['ap_standalone_page'] = 'Autonominis puslapis';
+$lang['Load a page model'] = 'Įkrauti puslapio modelį';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Puslapio turinys pakeistas, ar tikrai norite išeiti neišsaugoję pakeitimų ?';
+$lang['ap_add_page'] = 'Pridėti puslapį';
+$lang['ap_all_group'] = 'Visi';
+$lang['ap_all_lang'] = 'Visi';
+$lang['ap_authorized_group'] = 'Autorizuotos grupės';
+$lang['ap_authorized_users'] = 'Autorizuoti vartotojai';
+$lang['ap_conf_saved'] = 'Konfigūracija išsaugota.';
+$lang['ap_config'] = 'Konfigūracija';
+$lang['ap_convert_bbcode_ok'] = 'Konvertavimas atliktas sėkmingai';
+$lang['ap_create'] = 'Sukurti naują papildomą puslapį';
+$lang['ap_default_language'] = 'Pagal nutylėjimą';
+$lang['ap_delete'] = 'Pašalinti';
+$lang['ap_deleted_page'] = 'Puslapis pašalintas';
+$lang['ap_edit_page'] = 'Redaguoti puslapį';
+$lang['ap_group_perm'] = 'paga grupes';
+$lang['ap_guest'] = '(palikti tuščią viešiems puslapiams)';
+$lang['ap_homepage_tip'] = 'Pakeisti galerijos pagrindinį puslapį';
+$lang['ap_language_perm'] = 'pagal kalbą';
+$lang['ap_level_perm'] = 'pagal privatumo lygį';
+?>
