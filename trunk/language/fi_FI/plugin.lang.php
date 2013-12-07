986a804da1ad97258e39b812d97ccba0dff49b61
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-07 19:55:17 +0000

    [additional_pages] Add fi_FI, thanks to : jhellen
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@25828 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/fi_FI/plugin.lang.php
@@ -0,0 +1,65 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_parse_bbcode'] = 'Munna BBCode-sivut';
+$lang['ap_permalink_already_used'] = 'Permalinkki %s on jo sivun %s käytössä';
+$lang['ap_show_home'] = 'Näytä "takaisin kotiin" painike lisäsivuilla';
+$lang['ap_perm'] = 'Aktivoi auktorisoinninhallinta:';
+$lang['ap_show_edit'] = 'Näytä muokkaa llinkki palikka-valikossa';
+$lang['ap_show_menu'] = 'Näytä palikka-valikko';
+$lang['ap_user_perm'] = 'käyttäjätyypittäin';
+$lang['Load a page model'] = 'Lataa sivupohja';
+$lang['ap_convert_bbcode_ok'] = 'Muunnos toteutui onnistuneesti';
+$lang['ap_redirect'] = 'Uudelleenohjaa automaattisesti tallennetulle sivulle';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Sivua on muutettu, haluatko lopettaa tallentamatta muutokset?';
+$lang['ap_add_page'] = 'Lisää sivu';
+$lang['ap_authorized_group'] = 'Auktorisoidut ryhmät';
+$lang['ap_authorized_users'] = 'Auktorisoidut käyttäjät';
+$lang['ap_conf_saved'] = 'Asetukset tallennettu';
+$lang['ap_config'] = 'Asetukset';
+$lang['ap_no_name'] = 'Et täytä nimikenttää.';
+$lang['ap_standalone_page'] = 'Yksittäinen sivu';
+$lang['ap_standalone_tip'] = 'Sivu vaatii täyden HTML-rakenteen';
+$lang['ap_unselect_all'] = 'Poisvalitse kaikki';
+$lang['ap_save'] = 'Tallenna';
+$lang['ap_saved_page'] = 'Sivu tallennettu';
+$lang['ap_select_all'] = 'Valitse kaikki';
+$lang['ap_set_as_homepage'] = 'Aseta kotisivuksi';
+$lang['ap_create'] = 'Luo uusi lisäsivu';
+$lang['ap_menu_name'] = 'Valikon nimi';
+$lang['ap_modify'] = 'Muokkaa lisäsivua';
+$lang['ap_page_content'] = 'Sisältö';
+$lang['ap_page_lang'] = 'Kieli';
+$lang['ap_page_name'] = 'Sivujen nimet';
+$lang['ap_page_pos'] = 'Sijainti';
+$lang['ap_all_group'] = 'Kaikki';
+$lang['ap_all_lang'] = 'Kaikki';
+$lang['ap_default_language'] = 'Oletus';
+$lang['ap_delete'] = 'Poista';
+$lang['ap_deleted_page'] = 'Sivu poistettu';
+$lang['ap_edit_page'] = 'Muokkaa sivua';
+$lang['ap_group_perm'] = 'ryhmittäin';
+$lang['ap_guest'] = '(jätä tyhjäksi julkista sivua varten)';
+$lang['ap_homepage_tip'] = 'Korvaa gallerian kotisivu';
+$lang['ap_language_perm'] = 'kielittäin';
+$lang['ap_level_perm'] = 'yksityisyystasottain';
+?>
