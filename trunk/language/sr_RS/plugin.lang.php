68370f4ef19bfe7ca31331ac9e29309df8074406
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-03-30 04:06:45 +0000

    [additional_pages] Update sr_RS, thanks to : gpopac
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@28017 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/sr_RS/plugin.lang.php
@@ -0,0 +1,65 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ap_permalink_already_used'] = 'Трајна веза ка %s је већ употребљена на страници %s';
+$lang['ap_pos0'] = 'Унесите „0“ да се не приказује страница у менију';
+$lang['ap_redirect'] = 'Сам преусмери на сачуване странице';
+$lang['ap_show_edit'] = 'Прикажи везу за уређивање на блок менију';
+$lang['ap_show_home'] = 'Прикажи дугме за враћање на почетну страницу на додатним страницама';
+$lang['ap_show_menu'] = 'Прикажи као блок мени';
+$lang['ap_standalone_page'] = 'Самостална страница';
+$lang['ap_standalone_tip'] = 'Страница захтева целокупну ХТМЛ структуру';
+$lang['ap_unselect_all'] = 'Поништи избор';
+$lang['ap_user_perm'] = 'по типу корисника';
+$lang['ap_menu_name'] = 'Име менија';
+$lang['ap_modify'] = 'Измени још једну страницу';
+$lang['ap_no_name'] = 'Нисте попунили поље са називом.';
+$lang['ap_page_content'] = 'Садржај';
+$lang['ap_page_lang'] = 'Језик';
+$lang['ap_page_name'] = 'Име странице';
+$lang['ap_page_pos'] = 'Место';
+$lang['ap_parse_bbcode'] = 'Преведи странице написане у BBCode коду';
+$lang['ap_perm'] = 'Омогући управљање овлашћенима:';
+$lang['ap_save'] = 'Сачувај';
+$lang['ap_saved_page'] = 'Страница је сачувана';
+$lang['ap_select_all'] = 'Изабери све';
+$lang['ap_set_as_homepage'] = 'Постави као почетну страницу';
+$lang['Load a page model'] = 'Учитај модел странице';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Садржај странице је измењен. Да ли желите да изађете а да га не сачувате?';
+$lang['ap_add_page'] = 'Додај страницу';
+$lang['ap_all_group'] = 'Све';
+$lang['ap_all_lang'] = 'Све';
+$lang['ap_authorized_group'] = 'Овлашћене групе';
+$lang['ap_authorized_users'] = 'Овлашћени корисници';
+$lang['ap_conf_saved'] = 'Подешавање је сачувано.';
+$lang['ap_config'] = 'Подешавање';
+$lang['ap_convert_bbcode_ok'] = 'Претварање је успело';
+$lang['ap_create'] = 'Направи нову додатну страницу';
+$lang['ap_default_language'] = 'Подразумевано';
+$lang['ap_delete'] = 'Обриши';
+$lang['ap_deleted_page'] = 'Страница је обрисана';
+$lang['ap_edit_page'] = 'Уреди страницу';
+$lang['ap_group_perm'] = 'по групама';
+$lang['ap_guest'] = '(оставите празно за јавне странице)';
+$lang['ap_homepage_tip'] = 'Замените насловну страницу ваше галерије';
+$lang['ap_language_perm'] = 'по језику';
+$lang['ap_level_perm'] = 'по нивоу приватности';
