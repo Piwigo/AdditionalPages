a18cfb26bd4ae18fc6e50d7515121a8901f918dd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-26 06:10:23 +0000

    [additional_pages] Update de_DE, thanks to bigant
    git-svn-id: http://piwigo.org/svn/extensions/AdditionalPages@30615 68402e56-0260-453c-a942-63ccdbb3a9ee

--- trunk/language/de_DE/plugin.lang.php
+++ trunk/language/de_DE/plugin.lang.php
@@ -4,20 +4,20 @@ $lang['ap_add_page'] = 'Seite hinzufügen';
 
 $lang['ap_create'] = 'Neue Seite hinzufügen';
 $lang['ap_deleted_page'] = 'Seite gelöscht';
-$lang['ap_no_name'] = 'Es wurde kein Name vergeben.';
+$lang['ap_no_name'] = 'Es wurde kein Name vergeben';
 $lang['ap_saved_page'] = 'Seite gespeichert';
 $lang['ap_modify'] = 'Neue Seite bearbeiten';
 $lang['ap_all_lang'] = 'Alle';
 $lang['ap_all_group'] = 'Alle';
 
-$lang['ap_conf_saved'] = 'Einstellungen gespeichert.';
+$lang['ap_conf_saved'] = 'Konfiguration gespeichert.';
 
 $lang['ap_page_name'] = 'Seitenname';
 $lang['ap_page_lang'] = 'Sprache';
 $lang['ap_default_language'] = 'Standard';
 $lang['ap_page_pos'] = 'Position';
-$lang['ap_authorized_group'] = 'Autorisierte Gruppen';
-$lang['ap_authorized_users'] = 'Autorisierte Benutzer';
+$lang['ap_authorized_group'] = 'Berechtigte Gruppen';
+$lang['ap_authorized_users'] = 'Berechtigte Benutzer';
 $lang['ap_select_all'] = 'Alle auswählen';
 $lang['ap_unselect_all'] = 'Alle abwählen';
 $lang['ap_guest'] = '(Keine Auswahl für öffentliche Seite)';
@@ -28,25 +28,24 @@ $lang['ap_delete'] = 'Löschen';
 
 $lang['ap_config'] = 'Einstellungen';
 $lang['ap_menu_name'] = 'Menü-Name';
-$lang['ap_show_menu'] = 'Zeige Block-Menü';
+$lang['ap_show_menu'] = 'Block-Menü anzeigen';
 $lang['ap_redirect'] = 'Automatische Weiterleitung zur gespeicherten Seite';
-$lang['ap_show_home'] = 'Zeige "Zur Startseite"-Link auf zusätzlichen Seiten';
+$lang['ap_show_home'] = 'Link zur Startseite auf zusätzlichen Seiten anzeigen';
 $lang['ap_show_edit'] = 'Zeige Bearbeiten-Link im Block-Menü';
-$lang['ap_perm'] = 'Zugriffsverwaltung anhand von:';
-$lang['ap_group_perm'] = 'Gruppen';
-$lang['ap_user_perm'] = 'Benutzern';
+$lang['ap_perm'] = 'Zugriffsverwaltung aktivieren:';
+$lang['ap_group_perm'] = 'nach Gruppen';
+$lang['ap_user_perm'] = 'nach Benutzertyp';
 $lang['ap_parse_bbcode'] = 'Seiten mit BBCode konvertieren';
-$lang['ap_convert_bbcode_ok'] = 'Konvertierung abgeschlossen.';
+$lang['ap_convert_bbcode_ok'] = 'Konvertierung erfolgreich abgeschlossen';
 
-$lang['ap_edit_page'] = 'Eine Seite editieren';
+$lang['ap_edit_page'] = 'Eine Seite bearbeiten';
 $lang['ap_set_as_homepage'] = 'Als Homepage setzen';
 $lang['ap_standalone_page'] = 'Einzelne Seite';
-$lang['ap_homepage_tip'] = 'Die Homepage der Galerie ersetzen';
+$lang['ap_homepage_tip'] = 'Ihre Galerie-Homepage ersetzen';
 $lang['ap_standalone_tip'] = 'Die Seite benötigt eine vollständige HTML Struktur';
 $lang['ap_permalink_already_used'] = 'Permalink %s wird bereits von der Seite %s benutzt';
 $lang['ap_level_perm'] = 'nach Ebene der Privatsphäre';
 $lang['ap_language_perm'] = 'nach Sprache';
 
-$lang['Load a page model'] = 'Lade ein Seiten Modell';
-$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Der Inhalt der Seite wurde geändert, sind Sie sicher ohne Speichern weiter zu machen?';
-?>
+$lang['Load a page model'] = 'Seitenvorlage laden';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Der Inhalt der Seite wurde geändert. Beenden, ohne zu speichern?';
