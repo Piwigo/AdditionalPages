6259b808b3705a199c6d18c0bb3f03e7112ecf6c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-18 14:00:32 +0200

    Add ja_JP, thanks to Trumpet

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,65 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Load a page model'] = 'ページモデルを読み込む';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'ページのコンテンツが変更されていますが、保存せずに終了したいのですか？';
+$lang['ap_group_perm'] = 'グループで';
+$lang['ap_guest'] = '(公開ページのため空にしておく)';
+$lang['ap_language_perm'] = '言語で';
+$lang['ap_level_perm'] = 'プライバシーレベルで';
+$lang['ap_no_name'] = '名称フィールドは入力できません。';
+$lang['ap_permalink_already_used'] = '永続リンク %s は、ページ %s で既に使われています';
+$lang['ap_pos0'] = 'メニューにページを表示しないなら0';
+$lang['ap_standalone_page'] = '独立したページ';
+$lang['ap_user_perm'] = '利用者のタイプで';
+$lang['ap_select_all'] = 'すべてを選択する';
+$lang['ap_set_as_homepage'] = 'ホームページに設定する';
+$lang['ap_show_edit'] = 'ブロックメニューに編集リンクを表示する';
+$lang['ap_show_home'] = '追加のページに「ホームへ戻る」ボタンを表示する';
+$lang['ap_show_menu'] = 'ブロックメニューを表示する';
+$lang['ap_standalone_tip'] = 'ページは完全なHTML構造を要求しています';
+$lang['ap_unselect_all'] = 'すべての選択を外す';
+$lang['ap_menu_name'] = 'メニューの名称';
+$lang['ap_modify'] = '追加ページを変更する';
+$lang['ap_page_content'] = 'コンテンツ';
+$lang['ap_page_lang'] = '言語';
+$lang['ap_page_name'] = 'ページの名称';
+$lang['ap_page_pos'] = '位置';
+$lang['ap_parse_bbcode'] = 'BBCodeで書かれたページを変換する';
+$lang['ap_perm'] = '有効な認証管理:';
+$lang['ap_redirect'] = '保存されたページへ自動的にリダイレクトする';
+$lang['ap_save'] = '保存する';
+$lang['ap_saved_page'] = 'ページが保存されました';
+$lang['ap_add_page'] = 'ページを追加する';
+$lang['ap_all_group'] = 'すべて';
+$lang['ap_all_lang'] = 'すべて';
+$lang['ap_authorized_group'] = '認証されたグループ';
+$lang['ap_authorized_users'] = '認証された利用者';
+$lang['ap_conf_saved'] = '設定が保存されました。';
+$lang['ap_config'] = '設定';
+$lang['ap_convert_bbcode_ok'] = '変換が正しく実行されました。';
+$lang['ap_create'] = '新規の追加ページを作成する';
+$lang['ap_default_language'] = '規定';
+$lang['ap_delete'] = '削除';
+$lang['ap_deleted_page'] = 'ページが削除されました';
+$lang['ap_edit_page'] = 'ページを編集する';
+$lang['ap_homepage_tip'] = 'ギャラリーのホームページを置き換える';
