1c9d220d0a639cc4fa260d2fa3d052ed65a8b0ac
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-10-15 18:23:40 +0200

    Add he_IL, thanks to ymoran00

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['ap_standalone_tip'] = 'הדף מצריך מבנה HTMLמלא';
+$lang['ap_unselect_all'] = 'בטל בחירה להכל';
+$lang['ap_user_perm'] = 'לפי סוג משתמש';
+$lang['ap_menu_name'] = 'שם תפריט';
+$lang['ap_modify'] = 'שנה את הדף הנוסף';
+$lang['ap_no_name'] = 'לא ממלאים את שדה השם.';
+$lang['ap_page_content'] = 'תוכן';
+$lang['ap_page_lang'] = 'שפה';
+$lang['ap_page_name'] = 'שם הדף';
+$lang['ap_page_pos'] = 'מיקום';
+$lang['ap_parse_bbcode'] = 'המר דפים הכתובים ב-BBCode';
+$lang['ap_perm'] = 'הפעל ניהול הרשאות:';
+$lang['ap_permalink_already_used'] = 'קישור קבוע %s כבר בשימוש בדף %s';
+$lang['ap_pos0'] = '0 כדי לא להציג דף בתפריט';
+$lang['ap_redirect'] = 'העברה אוטומטית לדף שמור';
+$lang['ap_save'] = 'שמור';
+$lang['ap_saved_page'] = 'דף נשמר';
+$lang['ap_select_all'] = 'בחר הכל';
+$lang['ap_set_as_homepage'] = 'הגדר כדף בית';
+$lang['ap_show_edit'] = 'הצג קישור לעריכה בתפריט חסימה';
+$lang['ap_show_home'] = 'הצג כפתור "בחזרה לבית" בדפים נוספים';
+$lang['ap_show_menu'] = 'הצג תפריט חסימה';
+$lang['ap_standalone_page'] = 'דף עצמאי';
+$lang['Load a page model'] = 'טען מודל דף';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'תוכן הדף השתנה, האם אתה בטוח שאתה מעוניין לצאת ללא שמירה?';
+$lang['ap_add_page'] = 'הוסף דף';
+$lang['ap_all_group'] = 'הכל';
+$lang['ap_all_lang'] = 'הכל';
+$lang['ap_authorized_group'] = 'קבוצות מאושרות';
+$lang['ap_authorized_users'] = 'משתמשים מאושרים';
+$lang['ap_conf_saved'] = 'הגדרות נשמרו.';
+$lang['ap_config'] = 'הגדרות';
+$lang['ap_convert_bbcode_ok'] = 'הומר בהצלחה';
+$lang['ap_create'] = 'צור דף נוסף חדש';
+$lang['ap_default_language'] = 'ברירת-מחדל';
+$lang['ap_delete'] = 'מחק';
+$lang['ap_deleted_page'] = 'דף נמחק';
+$lang['ap_edit_page'] = 'ערוך דף';
+$lang['ap_group_perm'] = 'לפי קבוצות';
+$lang['ap_guest'] = '(השאר ריק לדף פומבי)';
+$lang['ap_homepage_tip'] = 'החלף את דף הבית של הגלריה שלך';
+$lang['ap_language_perm'] = 'לפי שפה';
+$lang['ap_level_perm'] = 'לפי רמת פומביות';
