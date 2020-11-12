cc5c6765d59cc4cd5fc9c4e44efb3e8be2550430
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-11-12 07:04:52 +0100

    Update eu_ES, thanks to bixerdo

---
+++ language/eu_ES/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['ap_standalone_tip'] = 'Orriak HTML egitura osoa eskatzen du';
+$lang['ap_unselect_all'] = 'Desautatu guztiak';
+$lang['ap_user_perm'] = 'erabiltzaile motagatik';
+$lang['ap_permalink_already_used'] = '%s permalinka dagoeneko %s orrian erabiltzen da';
+$lang['ap_menu_name'] = 'Menuaren izena';
+$lang['ap_modify'] = 'Aldatu orri gehigarria';
+$lang['ap_no_name'] = 'Ez duzu bete izenaren eremua.';
+$lang['ap_page_content'] = 'Edukia';
+$lang['ap_page_lang'] = 'Hizkuntza';
+$lang['ap_page_name'] = 'Orriaren izena';
+$lang['ap_page_pos'] = 'Kokapena';
+$lang['ap_parse_bbcode'] = 'Bihurtu BBCoderekin idatzitako orriak';
+$lang['ap_perm'] = 'Aktibatu baimenen kudeaketa:';
+$lang['ap_pos0'] = 'Ez erakutsi 0 orria menuan';
+$lang['ap_redirect'] = 'Birbideratu zuzenean gordetako orrira';
+$lang['ap_save'] = 'Gorde';
+$lang['ap_saved_page'] = 'Orria gorde da';
+$lang['ap_select_all'] = 'Hautatu dena';
+$lang['ap_set_as_homepage'] = 'Ezarri hasierako orri gisa';
+$lang['ap_show_edit'] = 'Erakutsi menuan editatzeko esteka';
+$lang['ap_show_home'] = 'Erakutsi "Itzuli etxera" botoia gehitutako orrietan';
+$lang['ap_show_menu'] = 'Erakutsi menua';
+$lang['ap_standalone_page'] = 'Orri autonomoa';
+$lang['Load a page model'] = 'Kargatu orri eredua';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Orriaren edukia aldatu egin da. Ziur gorde barik irten nahi duzula?';
+$lang['ap_add_page'] = 'Gehitu orri bat';
+$lang['ap_all_group'] = 'Dena';
+$lang['ap_all_lang'] = 'Denak';
+$lang['ap_authorized_group'] = 'Baimendutako taldeak';
+$lang['ap_authorized_users'] = 'Baimendutako erabiltzaileak';
+$lang['ap_conf_saved'] = 'Ezarpenak gorde egin dira.';
+$lang['ap_config'] = 'Ezarpenak';
+$lang['ap_convert_bbcode_ok'] = 'Bihurketa zuzen burutu da';
+$lang['ap_create'] = 'Sortu orri berri bat';
+$lang['ap_default_language'] = 'Lehenetsita';
+$lang['ap_delete'] = 'Ezabatu';
+$lang['ap_deleted_page'] = 'Orria ezabatu egin da';
+$lang['ap_edit_page'] = 'Editatu orria';
+$lang['ap_group_perm'] = 'taldeagatik';
+$lang['ap_guest'] = '(hutsik utzi orri publikoetarako)';
+$lang['ap_homepage_tip'] = 'Ordeztu galeriaren hasierako orria';
+$lang['ap_language_perm'] = 'hizkuntzagatik';
+$lang['ap_level_perm'] = 'pribatutasun mailagatik';
