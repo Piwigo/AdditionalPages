8a53e4c1ea846de55ff41a9a898cece05f58b216
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-08 12:58:20 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['ap_standalone_tip'] = 'Síðan þarf fulla HTML uppbyggingu';
+$lang['ap_unselect_all'] = 'Afvelja allt';
+$lang['ap_user_perm'] = 'eftir notendategund';
+$lang['ap_menu_name'] = 'Valmyndarheiti';
+$lang['ap_modify'] = 'Breyttu viðbótarsíðunni';
+$lang['ap_no_name'] = 'Þú fyllir ekki út nafnareitinn.';
+$lang['ap_page_content'] = 'Innihald';
+$lang['ap_page_lang'] = 'Tungumál';
+$lang['ap_page_name'] = 'Síðuheiti';
+$lang['ap_page_pos'] = 'Staða';
+$lang['ap_parse_bbcode'] = 'Umbreyta síðum sem skrifaðar eru með BBCode';
+$lang['ap_perm'] = 'Virkjaðu heimildastjórnun:';
+$lang['ap_permalink_already_used'] = 'Fasthlekkur %s er þegar notaður af síðu %s';
+$lang['ap_pos0'] = '0 til að sýna ekki síðu í valmyndinni';
+$lang['ap_redirect'] = 'Beindu sjálfkrafa á vistaða síðu';
+$lang['ap_save'] = 'Vista';
+$lang['ap_saved_page'] = 'Síða vistuð';
+$lang['ap_select_all'] = 'Velja allt';
+$lang['ap_set_as_homepage'] = 'Setja sem forsíðu';
+$lang['ap_show_edit'] = 'Sýna breytingartengil í blokkarvalmynd';
+$lang['ap_show_home'] = 'Sýna "á forsíðu" hnappinn á fleiri síðum';
+$lang['ap_show_menu'] = 'Sýna blokkarvalmynd';
+$lang['ap_standalone_page'] = 'Sjálfstæð síða';
+$lang['Load a page model'] = 'Hladdu síðulíkani';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Innihald síðunnar breyttist, ertu viss um að þú viljir hætta án þess að vista?';
+$lang['ap_add_page'] = 'Bæta við síðu';
+$lang['ap_all_group'] = 'Allar';
+$lang['ap_all_lang'] = 'Allar';
+$lang['ap_authorized_group'] = 'Heimilaðir hópar';
+$lang['ap_authorized_users'] = 'Heimilaðir notendur';
+$lang['ap_conf_saved'] = 'Stilling vistuð.';
+$lang['ap_config'] = 'Stilling';
+$lang['ap_convert_bbcode_ok'] = 'Umbreyting tókst';
+$lang['ap_create'] = 'Stofna nýja viðbótarsíðu';
+$lang['ap_default_language'] = 'Sjálfgefin';
+$lang['ap_delete'] = 'Eyða';
+$lang['ap_deleted_page'] = 'Síðu eytt';
+$lang['ap_edit_page'] = 'Breyta síðu';
+$lang['ap_group_perm'] = 'eftir hópum';
+$lang['ap_guest'] = '(skildu eftir autt fyrir opinbera síðu)';
+$lang['ap_homepage_tip'] = 'Skiptu um forsíðu á galleríinu þínu';
+$lang['ap_language_perm'] = 'eftir tungumáli';
+$lang['ap_level_perm'] = 'eftir friðhelgisréttindum';
