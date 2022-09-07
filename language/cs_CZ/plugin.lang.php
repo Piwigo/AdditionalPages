f5762ed7af7b710ca1895473a197d89744305c8a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-09-07 18:50:59 +0200

    Update cs_CZ, thanks to jjelinek

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -12,40 +12,40 @@ $lang['ap_all_group'] = 'Všechny';
 
 $lang['ap_conf_saved'] = 'Konfigurace byla uložena.';
 
-$lang['ap_page_name'] = 'Jméno stránky';
+$lang['ap_page_name'] = 'Název stránky';
 $lang['ap_page_lang'] = 'Jazyk';
-$lang['ap_default_language'] = 'Defaultní';
+$lang['ap_default_language'] = 'Výchozí';
 $lang['ap_page_pos'] = 'Pozice';
 $lang['ap_authorized_group'] = 'Authorizované skupiny';
 $lang['ap_authorized_users'] = 'Authorizovaní uživatelé';
 $lang['ap_select_all'] = 'Vybrat vše';
 $lang['ap_unselect_all'] = 'Odvybrat vše';
 $lang['ap_guest'] = '(nechat prázdné pro veřejnou stránku)';
-$lang['ap_pos0'] = '0 znamená neukazovat stránku v menu';
+$lang['ap_pos0'] = 'o znamená nezobrazovat stránku v menu';
 $lang['ap_page_content'] = 'Obsah';
 $lang['ap_save'] = 'Uložit';
 $lang['ap_delete'] = 'Smazat';
 
 $lang['ap_config'] = 'Konfigurace';
-$lang['ap_menu_name'] = 'Jméno menu';
+$lang['ap_menu_name'] = 'Název menu';
 $lang['ap_show_menu'] = 'Zobrazit blokové menu';
 $lang['ap_redirect'] = 'Automatické přesměrování na uloženou stránku';
 $lang['ap_show_home'] = 'Zobrazit "zpět domů" na stránkách';
-$lang['ap_show_edit'] = 'Zobrazit link na editaci v blokovém menu';
-$lang['ap_perm'] = 'Aktivace správy autorizací:';
+$lang['ap_show_edit'] = 'Zobrazit odkaz na editaci v blokovém menu';
+$lang['ap_perm'] = 'Aktivovat správu autorizací:';
 $lang['ap_group_perm'] = 'podle skupin';
-$lang['ap_user_perm'] = 'podle typů uživatelů';
+$lang['ap_user_perm'] = 'podle typu uživatele';
 $lang['ap_parse_bbcode'] = 'Převést stránky psané pomocí BBCode';
 $lang['ap_convert_bbcode_ok'] = 'Převod proběhl v pořádku.';
 
-$lang['ap_edit_page'] = 'Editace stránky';
+$lang['ap_edit_page'] = 'Úprava stránky';
 $lang['ap_set_as_homepage'] = 'Nastavit jako Hlavní stránku';
 $lang['ap_standalone_page'] = 'Samostatná stránka';
-$lang['ap_homepage_tip'] = 'Nahrazení hlavní stránky fotogalerie';
+$lang['ap_homepage_tip'] = 'Nahradit hlavní stránku fotogalerie';
 $lang['ap_standalone_tip'] = 'Stránka vyžaduje plnou HTML strukturu';
-$lang['ap_permalink_already_used'] = 'Tento Permanentní odkaz %s je už používán stránkou %s';
-$lang['ap_level_perm'] = 'podle úrovně ochrany osobních údajů';
+$lang['ap_permalink_already_used'] = 'Tento trvalý odkaz %s je už používán stránkou %s';
+$lang['ap_level_perm'] = 'podle úrovně soukromí';
 $lang['ap_language_perm'] = 'podle jazyka';
 
 $lang['Load a page model'] = 'Nahraj model stránky.';
-$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Obsah této strámky byl změněn, jste si jistý, že chcete odejít bez uložení změn?';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = 'Obsah této strámky byl změněn, opravdu chcete odejít bez uložení změn?';
