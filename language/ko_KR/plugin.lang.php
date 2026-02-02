aaa45ef0131e0959ded8c305abc369107bdcadbf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-02 06:22:29 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['ap_standalone_tip'] = '페이지에는 완전한 HTML 구조가 필요합니다';
+$lang['ap_unselect_all'] = '전체 선택 해제';
+$lang['ap_user_perm'] = '사용자 유형별';
+$lang['ap_menu_name'] = '메뉴 이름';
+$lang['ap_modify'] = '추가 페이지 수정';
+$lang['ap_no_name'] = '이름 필드를 입력하지 않았습니다.';
+$lang['ap_page_content'] = '내용';
+$lang['ap_page_lang'] = '언어';
+$lang['ap_page_name'] = '페이지 이름';
+$lang['ap_page_pos'] = '위치';
+$lang['ap_parse_bbcode'] = 'BBCode로 작성된 페이지 변환';
+$lang['ap_perm'] = '권한 관리 활성화:';
+$lang['ap_permalink_already_used'] = '퍼머링크 %s는 이미 페이지 %s에서 사용 중입니다.';
+$lang['ap_pos0'] = '0: 메뉴에 페이지 표시 안 함';
+$lang['ap_redirect'] = '저장된 페이지로 자동 리디렉션';
+$lang['ap_save'] = '저장';
+$lang['ap_saved_page'] = '페이지 저장되었습니다';
+$lang['ap_select_all'] = '모두 선택';
+$lang['ap_set_as_homepage'] = '홈페이지로 설정';
+$lang['ap_show_edit'] = '블록 메뉴에 편집 링크 표시';
+$lang['ap_show_home'] = '추가 페이지에 "홈으로 돌아가기" 버튼 표시';
+$lang['ap_show_menu'] = '블록 메뉴 표시';
+$lang['ap_standalone_page'] = '독립형 페이지';
+$lang['Load a page model'] = '페이지 모델 불러오기';
+$lang['The content of the page changed, are your sure you wan\'t to quit without saving?'] = '페이지 내용이 변경되었습니다. 저장하지 않고 종료하시겠습니까?';
+$lang['ap_add_page'] = '페이지 추가';
+$lang['ap_all_group'] = '전체';
+$lang['ap_all_lang'] = '전체';
+$lang['ap_authorized_group'] = '허가된 그룹';
+$lang['ap_authorized_users'] = '허가된 사용자';
+$lang['ap_conf_saved'] = '설정 완료되었습니다.';
+$lang['ap_config'] = '환경 설정';
+$lang['ap_convert_bbcode_ok'] = '변환이 성공적으로 수행되었습니다';
+$lang['ap_create'] = '새 추가 페이지 생성';
+$lang['ap_default_language'] = '기본값';
+$lang['ap_delete'] = '삭제';
+$lang['ap_deleted_page'] = '페이지가 삭제되었습니다';
+$lang['ap_edit_page'] = '페이지 편집';
+$lang['ap_group_perm'] = '그룹별';
+$lang['ap_guest'] = '(공개 페이지인 경우 비워 두기)';
+$lang['ap_homepage_tip'] = '갤러리 홈페이지 교체';
+$lang['ap_language_perm'] = '언어별';
+$lang['ap_level_perm'] = '개인정보 수준별';
